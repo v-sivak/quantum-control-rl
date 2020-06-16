bc19062191c9986bb442c62d4cb0ce2f576c6ef1
Vladimir Sivak <sivak.volodymyr@gmail.com>
2020-06-16 16:41:53 -0400

    Modified 'clock' observation.
    Now clock periodicity 'T' is not the same as history horizon 'H'.

--- gkp/gkp_tf_env/gkp_tf_env.py
+++ gkp/gkp_tf_env/gkp_tf_env.py
@@ -64,6 +64,7 @@ class GKP(tf_environment.TFEnvironment):
         # Default simulation parameters
         self.N = 100 # size of the oscillator Hilbert space truncation
         self.H = 1   # horizon for history returned in observations
+        self.T = 4   # periodicity of the 'clock' observation
         self.max_episode_length = None
         self.episode_length = 20
         self.batch_size = 50
@@ -107,7 +108,7 @@ class GKP(tf_environment.TFEnvironment):
             'beta'  : spec(self.H, 2), 
             'phi'   : spec(self.H, 1),
             'msmt'  : spec(self.H, 1),
-            'clock' : spec(self.H, self.H)}
+            'clock' : spec(self.H, self.T)}
         if self.quantum_circuit_type == 'v3': 
             observation_spec['epsilon'] = spec(self.H, 2)
         time_step_spec = ts.time_step_spec(observation_spec)
@@ -143,7 +144,7 @@ class GKP(tf_environment.TFEnvironment):
             self.history[a].append(tf.expand_dims(action[a], axis=1))
         self.history['msmt'].append(tf.expand_dims(obs, axis=1))
         # Also add clock of periodicity 'H' to history
-        C = tf.one_hot([[self._elapsed_steps%self.H]]*self.batch_size, self.H)
+        C = tf.one_hot([[self._elapsed_steps%self.T]]*self.batch_size, self.T)
         self.history['clock'].append(C)
         
         # Make observations of horizon H, shape=[batch_size,H,dim]
@@ -200,7 +201,7 @@ class GKP(tf_environment.TFEnvironment):
         self.history = tensor_spec.zero_spec_nest(self.action_spec(), 
                                       outer_dims=(self.batch_size,))
         self.history['msmt'] = tf.zeros(shape=[self.batch_size,1,1])
-        self.history['clock'] = tf.zeros([self.batch_size,1,self.H])
+        self.history['clock'] = tf.zeros([self.batch_size,1,self.T])
         for key in self.history.keys():
             self.history[key] = [self.history[key]]*self.H
         
