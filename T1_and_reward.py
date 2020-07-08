a226a2efc1af5d22e1f46aafeeaf241c358549d1
Vladimir Sivak <sivak.volodymyr@gmail.com>
2020-07-08 18:00:22 -0400

    Cleaned up 'evolution of stabilizers.py' and 'T1_and_reward.py'
    * Removed outdated features related to 'stabilizers' reward mode,
      and made a nicer plotting.
    * Removed fitting of T1 on measurement outcomes, now fitting only
      the fidelity reward  which works for all types of protocols and
      encodings.
    * Fixed a serious bug in T1 script: setting of the 'init' attribute
      doesn't work for wrapped policies! This was fooling me into thinking
      that the agent can make all logical lifetimes equal, while in fact
      it was always measuring the same lifetime because <env.init = something>
      didn't work properly. This was sad to find out.

--- T1_and_reward.py
+++ T1_and_reward.py
@@ -6,7 +6,6 @@ Created on Tue Apr  7 16:16:45 2020
 """
 
 import os
-os.environ["TF_MIN_GPU_MULTIPROCESSOR_COUNT"]="2"
 os.environ["TF_FORCE_GPU_ALLOW_GROWTH"]='true'
 os.environ["CUDA_VISIBLE_DEVICES"]="0"
 
@@ -24,49 +23,53 @@ from gkp.gkp_tf_env import gkp_init
 #-----------------------------------------------------------------------------
 #-----------------------------------------------------------------------------
 
-
-# env = gkp_init(simulate='oscillator_qubit', 
-#                 init='X+', H=1, batch_size=400, episode_length=24, 
-#                 reward_mode = 'fidelity', quantum_circuit_type='v2')
-
-
-# from gkp.action_script import phase_estimation_symmetric_with_trim_4round as action_script
-# to_learn = {'alpha':True, 'beta':True, 'phi':True}
-# env = wrappers.ActionWrapper(env, action_script, to_learn)
-# env = wrappers.FlattenObservationsWrapperTF(env)
-
-# root_dir = r'E:\VladGoogleDrive\Qulab\GKP\sims\PPO\OscillatorQubitGKP'
-# policy_dir = r'rnn_steps24_mask_quadrant_lr1e-4_v2\policy\002500000'
-# policy = tf.compat.v2.saved_model.load(os.path.join(root_dir,policy_dir))
-
-
-env = gkp_init(simulate='oscillator_qubit',
-                init='X+', H=1, batch_size=400, episode_length=24, 
-                reward_mode='fidelity', quantum_circuit_type='v2')
+env = gkp_init(simulate='oscillator', 
+               init='X+', H=4, T=4, batch_size=1000, episode_length=200, 
+               reward_mode='fidelity', quantum_circuit_type='v2',
+               encoding='square')
 
 from gkp.action_script import phase_estimation_symmetric_with_trim_4round as action_script
-policy = plc.ScriptedPolicy(env.time_step_spec(), action_script)
+to_learn = {'alpha':True, 'beta':True, 'phi':False}
+env = wrappers.ActionWrapper(env, action_script, to_learn)
+env = wrappers.FlattenObservationsWrapperTF(env,
+                        observations_whitelist=['msmt','clock'])
 
+root_dir = r'E:\VladGoogleDrive\Qulab\GKP\sims\PPO\July\OscillatorGKP'
+policy_dir = r'mlp3_steps36_48_lr1e-4_script_alpha_v2\policy\000100000'
+policy = tf.compat.v2.saved_model.load(os.path.join(root_dir,policy_dir))
+
+
+# # from gkp.action_script import hexagonal_phase_estimation_symmetric_6round as action_script
+# # from gkp.action_script import phase_estimation_symmetric_with_trim_4round as action_script
+# policy = plc.ScriptedPolicy(env.time_step_spec(), action_script)
 
 #-----------------------------------------------------------------------------
 #-----------------------------------------------------------------------------
 #-----------------------------------------------------------------------------
 
-states = ['X+','Y+', 'Z+'] #['X+', 'Y+', 'Z+']
+states = ['X+', 'Y+', 'Z+']
 results = {state : np.zeros(env.episode_length) for state in states}
-rewards = {state : np.zeros((env.episode_length, env.batch_size)) 
+rewards = {state : np.zeros((env.episode_length, env.batch_size))
            for state in states}
+
+assert env.reward_mode == 'fidelity'
+
 for state in states:
-    pauli = env.code_map[state[0]]
-    cache = [] # store intermediate states
-    env.init = state
+    if '_env' in env.__dir__(): 
+        env._env._env.init = state
+    else:
+        env.init = state
+
+    pauli = env.code_map[state[0]] # which Pauli to measure
+    cache = [] # store intermediate states (after feedback)
     
+    # Collect batch of episodes
     time_step = env.reset()
     policy_state = policy.get_initial_state(env.batch_size)
     counter = 0
     while not time_step.is_last()[0]:
         t = time()
-        action_step = policy.action(time_step, policy_state)      
+        action_step = policy.action(time_step, policy_state)
         policy_state = action_step.state
         time_step = env.step(action_step.action)
         cache.append(env.info['psi_cached'])
@@ -74,32 +77,8 @@ for state in states:
         counter += 1
         print('%d: Time %.3f sec' %(counter, time()-t))
 
-    # measure T1 using cached states
-    pauli_batch = tf.stack([pauli]*env.batch_size)
-    pauli_batch = tf.cast(pauli_batch, tf.complex64)
-    phi_batch = tf.stack([0.0]*env.batch_size)  
-    for j, psi in enumerate(cache):   
-        _, z = env.phase_estimation(psi, pauli_batch, phi_batch)
-        results[state][j] = np.mean(z)
-        
-# Plot T1
-fig, ax = plt.subplots(1,1)
-ax.set_title('T1')
-ax.set_xlabel('Time (us)')
-palette = plt.get_cmap('tab10')
-times = np.arange(env.episode_length)*env.step_duration
-for i, state in enumerate(states):
-    ax.plot(times*1e6, results[state], color=palette(i),
-            marker='.', linestyle='None')
-    popt, pcov = curve_fit(hf.exp_decay, times, np.abs(results[state]),
-                           p0=[1, env.T1_osc])
-    ax.plot(times*1e6, hf.exp_decay(times, popt[0], popt[1]), 
-            label = state + ' : %.2f us' %(popt[1]*1e6),
-            linestyle='--', color=palette(i))
-ax.legend()
 
-
-# Plot mean reward from every time step
+# Plot average reward from every time step and fit T1
 fig, ax = plt.subplots(1,1)
 ax.set_xlabel('Step')
 ax.set_ylabel('Reward')
@@ -107,16 +86,13 @@ palette = plt.get_cmap('tab10')
 steps = np.arange(env.episode_length)
 for i, state in enumerate(states):
     mean_rewards = rewards[state].mean(axis=1) # average across episodes
-    avg_return = np.sum(mean_rewards)
     ax.plot(steps, mean_rewards, color=palette(i),
-            label = state + ' return : %.2f' %avg_return, 
             linestyle='none', marker='.')
-    if env.reward_mode == 'fidelity':
-        times = steps*env.step_duration
-        popt, pcov = curve_fit(hf.exp_decay, times, mean_rewards,
-                               p0=[1, env.T1_osc])
-        ax.plot(steps, hf.exp_decay(times, popt[0], popt[1]), 
-                label = state + ' lifetime : %.2f us' %(popt[1]*1e6),
-                linestyle='--', color=palette(i))
-ax.set_title('Average return')
+    times = steps*env.step_duration
+    popt, pcov = curve_fit(hf.exp_decay, times, mean_rewards,
+                           p0=[1, env.T1_osc])
+    ax.plot(steps, hf.exp_decay(times, popt[0], popt[1]), 
+            label = state + ' : %.2f us' %(popt[1]*1e6),
+            linestyle='--', color=palette(i))
+ax.set_title('Logical lifetime')
 ax.legend()
--- evolution_of_stabilizers.py
+++ evolution_of_stabilizers.py
@@ -6,7 +6,6 @@ Created on Tue Apr  7 16:24:22 2020
 """
 
 import os
-os.environ["TF_MIN_GPU_MULTIPROCESSOR_COUNT"]="2"
 os.environ["TF_FORCE_GPU_ALLOW_GROWTH"]='true'
 os.environ["CUDA_VISIBLE_DEVICES"]="0"
 
@@ -23,27 +22,20 @@ from gkp.gkp_tf_env import gkp_init
 #-----------------------------------------------------------------------------
 #-----------------------------------------------------------------------------
 
-
 env = gkp_init(simulate='oscillator', 
-                init='X+', H=1, batch_size=600, episode_length=100, 
-                reward_mode = 'pauli', quantum_circuit_type='v2')
-
+                init='vac', H=4, T=4, batch_size=1000, episode_length=100, 
+                reward_mode='zero', quantum_circuit_type='v2')
 
 from gkp.action_script import phase_estimation_symmetric_with_trim_4round as action_script
-to_learn = {'alpha':True, 'beta':True, 'phi':True}
+to_learn = {'alpha':True, 'beta':True, 'phi':False}
 env = wrappers.ActionWrapper(env, action_script, to_learn)
 env = wrappers.FlattenObservationsWrapperTF(env,
                         observations_whitelist=['msmt','clock'])
 
 root_dir = r'E:\VladGoogleDrive\Qulab\GKP\sims\PPO\July\OscillatorGKP'
-policy_dir = r'rnn_steps36fixed_lr1e-4_v2\policy\000200000'
+policy_dir = r'mlp3_steps36_48_lr1e-4_script_alpha_v2\policy\000100000'
 policy = tf.compat.v2.saved_model.load(os.path.join(root_dir,policy_dir))
 
-
-
-# env = gkp_init(simulate='oscillator', 
-#                 init='random', H=1, batch_size=100, episode_length=100, 
-#                 reward_mode = 'stabilizers', quantum_circuit_type='v2')
 
 # from gkp.action_script import phase_estimation_symmetric_with_trim_4round as action_script
 # policy = plc.ScriptedPolicy(env.time_step_spec(), action_script)
@@ -52,15 +44,23 @@ policy = tf.compat.v2.saved_model.load(os.path.join(root_dir,policy_dir))
 #-----------------------------------------------------------------------------
 #-----------------------------------------------------------------------------
 
-names = ['Re(S_x)', 'Im(S_x)', 'Re(S_z)', 'Im(S_z)']
-stabilizers = [env.code_map['S_x'], env.code_map['S_x'], 
-               env.code_map['S_z'], env.code_map['S_z']]
-angles = [0, -pi/2, 0, -pi/2]
-results = {name : np.zeros(env.episode_length) for name in names}
-cache = [] # store intermediate states
-reward_cache = []
-times = []
+# What operators to measure 
+names = [r'Re($S_x$)', r'Re($S_y$)', r'Re($S_z$)',
+         r'Im($S_x$)', r'Im($S_y$)', r'Im($S_z$)']
+# Translation amplitudes
+stabilizers = [env.code_map[S] for S in ['S_x','S_y','S_z']*2]
+# Qubit measurement angles
+angles = [0]*3 + [-pi/2]*3
+# Params for plotting
+lines = ['-']*3 + ['--']*3
+palette = plt.get_cmap('tab10')
+colors = [palette(0), palette(1), palette(2)]*2
 
+results = {name : np.zeros(env.episode_length) for name in names}
+cache = [] # store intermediate states (after feedback)
+times = [] # for timing every round
+
+# Collect batch of episodes
 time_step = env.reset()
 policy_state = policy.get_initial_state(env.batch_size)
 counter = 0
@@ -72,10 +72,10 @@ while not time_step.is_last()[0]:
     time_step = env.step(action_step.action)
     t_stop = time()
     cache.append(env.info['psi_cached'])
-    reward_cache.append(np.mean(time_step.reward))
-    times.append(t_stop-t_start)
+    times.append(t_stop - t_start)
     print('%d: Time %.3f sec' %(counter, times[-1]))
 
+# Measure stabilizers on cached wavefunctions
 for name, stabilizer, phi in zip(names, stabilizers, angles):
     for i, psi in enumerate(cache):
         stabilizer_batch = tf.stack([stabilizer]*env.batch_size)
@@ -89,11 +89,7 @@ fig, ax = plt.subplots(1,1)
 ax.set_xlabel('Step')
 ax.set_title('Stabilizers')
 steps = np.arange(env.episode_length)
-for name in names:
-    ax.plot(steps, results[name], label = name)
-
-# Plot average reward from reward-collecting rounds only
-reward_cache = np.array(reward_cache)
-ind = np.where(reward_cache!=0)[0]
-ax.plot(steps[ind], reward_cache[ind], color='k')
+for i, name in enumerate(names):
+    ax.plot(steps, results[name], label=name, 
+            linestyle=lines[i], color=colors[i])
 ax.legend()
