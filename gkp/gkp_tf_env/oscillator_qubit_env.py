475ece56bbaa4ef532f33d2144ff997666e0eacd
Henry Liu <henry@liuhenry.com>
2020-08-19 16:55:22 -0400

    Fix autograph failure in oscillator_qubit
    Autograph isn't able to trace functions with a backslash continuation,
    see: https://github.com/tensorflow/tensorflow/issues/35765

--- gkp/gkp_tf_env/oscillator_qubit_env.py
+++ gkp/gkp_tf_env/oscillator_qubit_env.py
@@ -57,24 +57,24 @@ class OscillatorQubitGKP(OscillatorQubit, BatchOperatorMixinBCH, GKP):
     @tf.function
     def quantum_circuit_v1(self, psi, action):
         """
-        Apply sequenct of quantum gates version 1. In this version conditional 
-        translation by 'beta' is not symmetric (translates if qubit is in '1') 
-        
+        Apply sequenct of quantum gates version 1. In this version conditional
+        translation by 'beta' is not symmetric (translates if qubit is in '1')
+
         Input:
             action -- dictionary of batched actions. Dictionary keys are
                       'alpha', 'beta', 'phi'
-            
+
         Output:
             psi_final -- batch of final states; shape=[batch_size,N]
             psi_cached -- batch of cached states; shape=[batch_size,N]
             obs -- measurement outcomes; shape=(batch_size,)
-            
+
         """
         # Extract parameters
         alpha = hf.vec_to_complex(action['alpha'])
         beta = hf.vec_to_complex(action['beta'])
         phi = action['phi']
-        
+
         # Construct gates
         Hadamard = tf.stack([self.hadamard]*self.batch_size)
         sx = tf.stack([self.sx]*self.batch_size)
@@ -103,27 +103,27 @@ class OscillatorQubitGKP(OscillatorQubit, BatchOperatorMixinBCH, GKP):
         # Feedback delay
         psi = self.simulate(psi, self.t_delay)
         # Flip qubit conditioned on the measurement
-        psi_final = psi * tf.cast((obs==1), c64) \
-            + batch_dot(sx, psi) * tf.cast((obs==-1), c64)
+        psi_final = psi * tf.cast((obs==1), c64)
+        psi_final += batch_dot(sx, psi) * tf.cast((obs==-1), c64)
 
         return psi_final, psi_cached, obs
 
     @tf.function
     def quantum_circuit_v2(self, psi, action):
         """
-        Apply sequenct of quantum gates version 2. In this version conditional 
-        translation by 'beta' is symmetric (translates by +-beta/2 controlled 
+        Apply sequenct of quantum gates version 2. In this version conditional
+        translation by 'beta' is symmetric (translates by +-beta/2 controlled
         by the qubit)
-        
+
         Input:
             action -- dictionary of batched actions. Dictionary keys are
                       'alpha', 'beta', 'phi'
-            
+
         Output:
             psi_final -- batch of final states; shape=[batch_size,N]
             psi_cached -- batch of cached states; shape=[batch_size,N]
             obs -- measurement outcomes; shape=(batch_size,)
-            
+
         """
         # Extract parameters
         alpha = hf.vec_to_complex(action['alpha'])
@@ -160,8 +160,8 @@ class OscillatorQubitGKP(OscillatorQubit, BatchOperatorMixinBCH, GKP):
         # Feedback delay
         psi = self.simulate(psi, self.t_delay)
         # Flip qubit conditioned on the measurement
-        psi_final = psi * tf.cast((obs==1), c64) \
-            + batch_dot(sx, psi) * tf.cast((obs==-1), c64)
+        psi_final = psi * tf.cast((obs==1), c64)
+        psi_final += batch_dot(sx, psi) * tf.cast((obs==-1), c64)
 
         return psi_final, psi_cached, obs
 
@@ -169,19 +169,19 @@ class OscillatorQubitGKP(OscillatorQubit, BatchOperatorMixinBCH, GKP):
     @tf.function
     def quantum_circuit_v3(self, psi, action):
         """
-        Apply sequenct of quantum gates version 3. This is a protocol proposed 
-        by Baptiste. It essentially combines trimming and sharpening in a 
+        Apply sequenct of quantum gates version 3. This is a protocol proposed
+        by Baptiste. It essentially combines trimming and sharpening in a
         single round. Trimming is controlled by 'epsilon'.
-        
+
         Input:
             action -- dictionary of batched actions. Dictionary keys are
                       'alpha', 'beta', 'epsilon', 'phi'
-            
+
         Output:
             psi_final -- batch of final states; shape=[batch_size,N]
             psi_cached -- batch of cached states; shape=[batch_size,N]
             obs -- measurement outcomes; shape=(batch_size,)
-            
+
         """
         # extract parameters
         alpha = hf.vec_to_complex(action['alpha'])
@@ -231,8 +231,8 @@ class OscillatorQubitGKP(OscillatorQubit, BatchOperatorMixinBCH, GKP):
         # Feedback delay
         psi = self.simulate(psi, self.t_delay)
         # Flip qubit conditioned on the measurement
-        psi_final = psi * tf.cast((obs==1), c64) \
-            + batch_dot(sx, psi) * tf.cast((obs==-1), c64)
+        psi_final = psi * tf.cast((obs==1), c64)
+        psi_final += batch_dot(sx, psi) * tf.cast((obs==-1), c64)
 
         return psi_final, psi_cached, obs
 
@@ -240,21 +240,21 @@ class OscillatorQubitGKP(OscillatorQubit, BatchOperatorMixinBCH, GKP):
     @tf.function
     def quantum_circuit_v4(self, psi, action):
         """
-        Apply sequence of quantum gates version 4. This is a protocol proposed 
-        by Baptiste. It essentially combines trimming and sharpening in a 
-        single round. Trimming is controlled by 'epsilon'. This is similar to 
-        'v3', but the last conditional displacement gate is replaced with 
+        Apply sequence of quantum gates version 4. This is a protocol proposed
+        by Baptiste. It essentially combines trimming and sharpening in a
+        single round. Trimming is controlled by 'epsilon'. This is similar to
+        'v3', but the last conditional displacement gate is replaced with
         classicaly conditioned feedback.
-        
+
         Input:
             action -- dictionary of batched actions. Dictionary keys are
                       'alpha', 'beta', 'epsilon'
-            
+
         Output:
             psi_final -- batch of final states; shape=[batch_size,N]
             psi_cached -- batch of cached states; shape=[batch_size,N]
             obs -- measurement outcomes; shape=(batch_size,)
-            
+
         """
         # extract parameters
         alpha = hf.vec_to_complex(action['alpha'])
@@ -295,8 +295,8 @@ class OscillatorQubitGKP(OscillatorQubit, BatchOperatorMixinBCH, GKP):
         # Feedback delay
         psi = self.simulate(psi, self.t_delay)
         # Flip qubit conditioned on the measurement
-        psi_final = psi * tf.cast((obs==1), c64) \
-            + batch_dot(sx, psi) * tf.cast((obs==-1), c64)
+        psi_final = psi * tf.cast((obs==1), c64)
+        psi_final += batch_dot(sx, psi) * tf.cast((obs==-1), c64)
 
         return psi_final, psi_cached, obs
 
@@ -304,26 +304,26 @@ class OscillatorQubitGKP(OscillatorQubit, BatchOperatorMixinBCH, GKP):
     @tf.function # TODO: add losses in phase estimation?
     def phase_estimation(self, psi, beta, angle, sample=False):
         """
-        One round of phase estimation. 
-        
+        One round of phase estimation.
+
         Input:
             psi -- batch of state vectors; shape=[batch_size,2N]
             beta -- translation amplitude. shape=(batch_size,)
             angle -- angle along which to measure qubit. shape=(batch_size,)
             sample -- bool flag to sample or return expectation value
-        
+
         Output:
-            psi -- batch of collapsed states if sample==True, otherwise same 
+            psi -- batch of collapsed states if sample==True, otherwise same
                    as input psi; shape=[batch_size,2N]
             z -- batch of measurement outcomes if sample==True, otherwise
                  batch of expectation values of qubit sigma_z.
-                 
+
         """
         I = tf.stack([self.I]*self.batch_size)
         CT = self.ctrl(I, self.translate(beta))
         Phase = self.ctrl(I, self.phase(angle)*I)
         Hadamard = tf.stack([self.hadamard]*self.batch_size)
-        
+
         psi = batch_dot(Hadamard, psi)
         psi = batch_dot(CT, psi)
         psi = batch_dot(Phase, psi)
@@ -331,18 +331,18 @@ class OscillatorQubitGKP(OscillatorQubit, BatchOperatorMixinBCH, GKP):
         psi = normalize(psi)
         return self.measure(psi, self.P)
 
-    
+
 
     @tf.function
     def ctrl(self, U0, U1):
         """
-        Batch controlled-U gate. Apply 'U0' if qubit is '0', and 'U1' if 
+        Batch controlled-U gate. Apply 'U0' if qubit is '0', and 'U1' if
         qubit is '1'.
-        
+
         Input:
-            U0 -- unitary on the oscillator subspace written in the combined 
+            U0 -- unitary on the oscillator subspace written in the combined
                   qubit-oscillator Hilbert space; shape=[batch_size,2N,2N]
             U1 -- same as above
-                  
+
         """
         return self.P[0] @ U0 + self.P[1] @ U1
