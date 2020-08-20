a21893d75eba90a7ec2fcb782e7cbd0007eec3b4
Henry Liu <henry@liuhenry.com>
2020-08-20 15:04:57 -0400

    Update to tf==2.2.0, tfp==0.10.1, tf-agents==0.5.0
    This allows us to use the new profiling tools in TF 2.2.0

--- gkp/gkp_tf_env/tf_env_wrappers.py
+++ gkp/gkp_tf_env/tf_env_wrappers.py
@@ -6,29 +6,29 @@ Created on Wed Mar 18 20:10:01 2020
 """
 import numpy as np
 import tensorflow as tf
-from tf_agents.environments.tf_wrappers import TFEnvironmentBaseWrapper
+from numpy import pi
+from tensorflow.math import real, imag
 from tf_agents import specs
-from numpy import pi, sqrt
 from tf_agents.utils import common, nest_utils
 from tf_agents.specs import tensor_spec
 from tf_agents.trajectories import time_step as ts
+from tf_agents.environments.tf_wrappers import TFEnvironmentBaseWrapper
 
-from tensorflow_core._api.v2.math import real, imag
 
 class FlattenObservationsWrapperTF(TFEnvironmentBaseWrapper):
     """
     This is the same wrapper as FlattenObservationsWrapper from
-    tf_agents/environments/wrappers but I changed a few things to make it 
-    compatible with tf environments. The code is essentially copied with 
+    tf_agents/environments/wrappers but I changed a few things to make it
+    compatible with tf environments. The code is essentially copied with
     minor chages. See docs from that original class.
-    
+
     """
     def __init__(self, env, observations_whitelist=None):
-        
+
         super(FlattenObservationsWrapperTF, self).__init__(env)
         # If observations whitelist is provided:
         #  Check that the environment returns a dictionary of observations.
-        #  Check that the set of whitelist keys is a found in environment keys.       
+        #  Check that the set of whitelist keys is a found in environment keys.
         if observations_whitelist is not None:
             if not isinstance(env.observation_spec(), dict):
                 raise ValueError(
@@ -45,7 +45,7 @@ class FlattenObservationsWrapperTF(TFEnvironmentBaseWrapper):
                     set(observations_whitelist).difference(
                         env.observation_spec().keys())))
 
-        # Check that all observations have the same dtype. This dtype will be 
+        # Check that all observations have the same dtype. This dtype will be
         # used to create the flattened TensorSpec.
         env_dtypes = list(
             set([obs.dtype for obs in env.observation_spec().values()]))
@@ -61,9 +61,9 @@ class FlattenObservationsWrapperTF(TFEnvironmentBaseWrapper):
         if self._observations_whitelist is not None:
             observations_spec = self._filter_observations(observations_spec)
 
-        # Compute the observation length after flattening the observation items 
+        # Compute the observation length after flattening the observation items
         # and nested structure. Observation specs are not batched.
-        observation_total_len = sum(np.prod(observation.shape) for observation 
+        observation_total_len = sum(np.prod(observation.shape) for observation
                                     in tf.nest.flatten(observations_spec))
 
         # Update the observation spec as a tensor of one-dimension.
@@ -72,7 +72,7 @@ class FlattenObservationsWrapperTF(TFEnvironmentBaseWrapper):
             dtype=self._observation_spec_dtype,
             name='packed_observations')
 
-    def _flatten_nested_observations(self, observations, is_batched): 
+    def _flatten_nested_observations(self, observations, is_batched):
 
         def tf_flatten(x):
             if is_batched:
@@ -111,7 +111,7 @@ class FlattenObservationsWrapperTF(TFEnvironmentBaseWrapper):
 
     def _reset(self):
         return self._pack_and_filter_timestep_observation(self._env.reset())
-    
+
     def _step(self, action):
         return self._pack_and_filter_timestep_observation(self._env.step(action))
 
@@ -119,14 +119,14 @@ class FlattenObservationsWrapperTF(TFEnvironmentBaseWrapper):
 
 
 class ActionWrapper(TFEnvironmentBaseWrapper):
-    """    
-    Wrapper produces a dictionary with action components such as 'alpha', 
+    """
+    Wrapper produces a dictionary with action components such as 'alpha',
     'beta', 'epsilon', 'phi' as dictionary keys. Some action components are
-    taken from the action script provided at initialization, and some are 
+    taken from the action script provided at initialization, and some are
     taken from the input action produced by the agent. Parameter 'to_learn'
     controls which action components are to be learned. It is also possible
     to alternate between learned and scripted values with 'use_mask' flag.
-    
+
     """
     def __init__(self, env, action_script, to_learn, use_mask=True):
         """
@@ -134,25 +134,25 @@ class ActionWrapper(TFEnvironmentBaseWrapper):
             env: GKP environmen
             action_script: module or class with attributes corresponding to
                            action components such as 'alpha', 'phi' etc
-            to_learn: dictionary of bool values for action components 
+            to_learn: dictionary of bool values for action components
             use_mask: flag to control masking of action components
-            
-        """    
+
+        """
         super(ActionWrapper, self).__init__(env)
 
         self.scale = {'alpha' : 1, 'beta' : 1, 'epsilon' : 1, 'phi' : pi,
                       'theta' : 0.1}
-        self.dims_map = {'alpha' : 2, 'beta' : 2, 'epsilon' : 2, 
+        self.dims_map = {'alpha' : 2, 'beta' : 2, 'epsilon' : 2,
                          'phi' : 1, 'theta' : 1}
         self.period = action_script.period # periodicity of the protocol
         self.to_learn = to_learn
         self.use_mask = use_mask
         self.mask = action_script.mask
-        
+
         self._action_spec = {a : specs.BoundedTensorSpec(
             shape=[self.dims_map[a]], dtype=tf.float32, minimum=-1, maximum=1)
             for a, C in to_learn.items() if C}
-        
+
         # load the script of actions and convert to tensors
         self.script = action_script.script
         for a, val in self.script.items():
@@ -164,18 +164,18 @@ class ActionWrapper(TFEnvironmentBaseWrapper):
         """
         Args:
             input_action (dict): nested tensor action produced by the neural
-                                 net. Dictionary keys are those marked True 
+                                 net. Dictionary keys are those marked True
                                  in 'to_learn'.
-            
+
         Returns:
-            actions (dict): nested tensor action which includes all action 
-                            components expected by the GKP class. 
-            
+            actions (dict): nested tensor action which includes all action
+                            components expected by the GKP class.
+
         """
         # step counter to follow the script of periodicity 'period'
         i = self._env._elapsed_steps % self.period
         out_shape = nest_utils.get_outer_shape(input_action, self._action_spec)
-        
+
         action = {}
         for a in self.to_learn.keys():
             C1 = self.use_mask and self.mask[a][i]==0
@@ -190,24 +190,22 @@ class ActionWrapper(TFEnvironmentBaseWrapper):
                 action[a] = input_action[a]*self.scale[a]
 
         return action
-    
+
     def action_spec(self):
         return self._action_spec
 
     def _step(self, action):
         """
-        Take the nested tensor 'action' produced by the neural net and wrap it 
+        Take the nested tensor 'action' produced by the neural net and wrap it
         into dictionary format expected by the environment.
-        
-        Residual feedback learning trick: multiply the neural net prediction 
-        of 'alpha' by the measurement outcome of the last time step. This 
-        ensures that the Markovian part of the feedback is present, and the 
+
+        Residual feedback learning trick: multiply the neural net prediction
+        of 'alpha' by the measurement outcome of the last time step. This
+        ensures that the Markovian part of the feedback is present, and the
         agent can focus its efforts on learning residual part.
-        
+
         """
         action = self.wrap(action)
         m = self._env.current_time_step().observation['msmt'][:,-1,:]
         action['alpha'] *= m
         return self._env.step(action)
-
-
