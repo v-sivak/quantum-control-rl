794781d4654989eedf5f16efed12e32fc52e7329
Vladimir Sivak <vladimir.sivak@yale.edu>
2020-11-16 18:43:53 -0500

    Fix threshold in blind_fidelity experiment and improve plotting.

--- fpga_exp/blind_fidelity.py
+++ fpga_exp/blind_fidelity.py
@@ -14,8 +14,7 @@ class blind_fidelity(FPGAExperiment):
     delay_time = IntParameter(5e3)
     loop_delay= IntParameter(1e6)
     flip_qubit = BoolParameter(False)
-    threshold_g = FloatParameter(0)
-    threshold_e = FloatParameter(0)
+    threshold = FloatParameter(0)
 
     def sequence(self):
 
@@ -32,7 +31,8 @@ class blind_fidelity(FPGAExperiment):
         delay(self.loop_delay)
 
     def update(self):
-        readout.set_envelope(self.results['opt_env'].data)
+        readout.set_envelope(envelope0=self.results['opt_env'].data, 
+                             thresh0=self.threshold)
 
     def plot(self, fig, data):
 
@@ -43,7 +43,8 @@ class blind_fidelity(FPGAExperiment):
         ax1.get_xaxis().set_visible(False)
         ax1.plot(np.arange(len(e)), e.real, label='Re')
         ax1.plot(np.arange(len(e)), e.imag, label='Im')
-        ax1.plot(np.arange(len(e)), np.abs(e), label='Abs')
+        ax1.plot(np.arange(len(e)), np.abs(e), label='abs')
+        ax1.legend(loc='upper right')
 
         # Plot mean g trajectory after postselection
         ax2 = fig.add_subplot(323, sharex=ax1)
@@ -52,7 +53,8 @@ class blind_fidelity(FPGAExperiment):
         ax2.get_xaxis().set_visible(False)
         ax2.plot(np.arange(len(g)), g.real, label='Re')
         ax2.plot(np.arange(len(g)), g.imag, label='Im')
-        ax2.plot(np.arange(len(e)), np.abs(g), label='Abs')
+        ax2.plot(np.arange(len(e)), np.abs(g), label='abs')
+        ax2.legend(loc='upper right')
 
         # Plot the optimal demodulation envelope
         ax3 = fig.add_subplot(325, sharex=ax1)
@@ -60,14 +62,16 @@ class blind_fidelity(FPGAExperiment):
         ax3.set_title('optimal envelope')
         ax3.plot(np.arange(len(g)), env.real, label='Re')
         ax3.plot(np.arange(len(g)), env.imag, label='Im')
-        ax3.plot(np.arange(len(e)), np.abs(env), label='Abs')
+        ax3.plot(np.arange(len(e)), np.abs(env), label='abs')
+        ax3.legend(loc='upper right')
 
         # Plot histogram of 'e'
         ax4 = fig.add_subplot(322)
-        e = (data['traj_e'].data[:,1,:]*env).sum(axis=1) # use only the second measurement
-        e_post = (data['postselected_e'].data*env).sum(axis=1)
-        fidelity = np.sum(e>0)/float(len(e))
-        blind_fidelity = np.sum(e_post>0)/float(len(e_post))
+        ax4.set_title('e')
+        e = (data['traj_e'].data[:,1,:]*env).mean(axis=1) # use only the second measurement
+        e_post = (data['postselected_e'].data*env).mean(axis=1)
+        fidelity = np.sum(e>self.threshold) / float(len(e))
+        blind_fidelity = np.sum(e_post>self.threshold) / float(len(e_post))
         bins = 100
         binedges = np.linspace(min(e),max(e),bins+1)
         bincenters = [np.mean(binedges[i:i+1]) for i in range(len(binedges)-1)]
@@ -75,14 +79,16 @@ class blind_fidelity(FPGAExperiment):
         H_e_post, _ = np.histogram(e_post, bins=binedges)
         ax4.plot(bincenters, H_e, label='%.3f'%fidelity)
         ax4.plot(bincenters, H_e_post, label='%.3f'%blind_fidelity)
+        ax4.plot([self.threshold]*2, [0, np.max(H_e)])
         ax4.legend()
 
         # Plot histogram of 'g'
         ax5 = fig.add_subplot(324, sharex=ax4)
-        g = (data['traj_g'].data[:,1,:]*env).sum(axis=1)
-        g_post = (data['postselected_g'].data*env).sum(axis=1)
-        fidelity = np.sum(g<0)/float(len(g))
-        blind_fidelity = np.sum(g_post<0)/float(len(g_post))
+        ax5.set_title('g')
+        g = (data['traj_g'].data[:,1,:]*env).mean(axis=1)
+        g_post = (data['postselected_g'].data*env).mean(axis=1)
+        fidelity = np.sum(g<self.threshold) / float(len(g))
+        blind_fidelity = np.sum(g_post<self.threshold) / float(len(g_post))
         bins = 100
         binedges = np.linspace(min(g),max(g),bins+1)
         bincenters = [np.mean(binedges[i:i+1]) for i in range(len(binedges)-1)]
@@ -90,6 +96,7 @@ class blind_fidelity(FPGAExperiment):
         H_g_post, _ = np.histogram(g_post, bins=binedges)
         ax5.plot(bincenters, H_g, label='%.3f'%fidelity)
         ax5.plot(bincenters, H_g_post, label='%.3f'%blind_fidelity)
+        ax5.plot([self.threshold]*2, [0, np.max(H_g)])
         ax5.legend()
 
     def process_data(self):
@@ -98,15 +105,14 @@ class blind_fidelity(FPGAExperiment):
         use them to define a better envelope and measure "blind" fidelity of readout.
 
         """
-        thresh_g = self.threshold_g
-        thresh_e = self.threshold_e
+        threshold = self.threshold
         mean = {}
         for s in ['g', 'e']:
             postselected = [] # list of indices which should be postselected
             state = self.results['state_'+s].data
             N = len(state)
             for i in range(int(N)):
-                if (s=='e' and state[i,0]>thresh_e) or (s=='g' and state[i,0]<thresh_g):
+                if (s=='e' and state[i,0]>threshold) or (s=='g' and state[i,0]<threshold):
                     postselected.append(i)
             data_postselected = self.results['traj_'+s].data[postselected,1,:]
             self.results.create('postselected_'+s, data_postselected)
