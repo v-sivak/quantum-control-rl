7c937629eca06447976c9d386196f9b4a7826bf1
Vladimir Sivak <sivak.volodymyr@gmail.com>
2020-11-28 12:14:46 -0500

    Start transition to new simulator API. This breaks compatibility.
    This will be followed by another commit that fixes up some of the
    code to make sure SNAP circuit can run without errors, and then I
    will patch up the rest of the code as things come up. Probably I
    should have done this in a separate branch, but who cares.
    
    This commit:
    * New simulator/operators.py with LinearOperator API.
    * New simulator/utils.py

--- simulator/operators.py
+++ simulator/operators.py
@@ -1,119 +1,375 @@
+# -*- coding: utf-8 -*-
 """
-Common operators for Quantum Harmonic Oscillator. Function names follow QuTIP.
+Created on Mon Nov 23 22:38:34 2020
 
-Created on Sun Jul 26 20:55:36 2020
-
-@author: Henry Liu
+@author: Vladimir Sivak
 """
-from distutils.version import LooseVersion
-
 import tensorflow as tf
-from math import pi
+from tensorflow import complex64 as c64
 
+from distutils.version import LooseVersion
 if LooseVersion(tf.__version__) >= "2.2":
     diag = tf.linalg.diag
 else:
     import numpy as np
-
     diag = np.diag  # k=1 option is broken in tf.linalg.diag in TF 2.1 (#35761)
 
+"""
+Operators are wrapped as instances of tf.linalg.LinearOperator which gives 
+access to a powerful API with a whole bunch of useful features, such as:
 
-def identity(N, dtype=tf.complex64):
+Attributes: shape, batch_shape, domain_dimension, is_self_adjoint, ...
+
+Methods: adjoint, to_dense, eigvals, matmul, matvec, inverse, ...
+
+More detailed documentation:
+    https://www.tensorflow.org/api_docs/python/tf/linalg/LinearOperator
+
+States are represented as simple c64 batched tensors of shape=[B1, .., Bb, N].
+
+Example 1:
+    
+    I = identity(2) # identity on the qubit
+    a_dag = create(100) # creation operator on the oscillator
+    # construct an operator on a joint Hilbert space and act with it on vacuum
+    fock1 = tf.linalg.LinearOperatorKronecker([I, a_dag]).matvec(vac)
+
+Example 2:
+    # Normalize states like so: 
+    normalized_state, norm = normalize(state)
+    
+"""
+
+# TODO: maybe re-define matvec to include explicit normalization
+# TODO: have different decorators for batched and un-batched operators
+# TODO: add is_self_adjoint attribute to be passed to the wrapper
+def linear_operator(func):
+    """
+    Wrap the output of <func> into tf.linalg.LinearOperator object.
+    
+    """
+    def wrapper(*args, **kwargs):
+        name = kwargs['name'] if 'name' in kwargs.keys() else func.__name__
+        operator_matrix = func(*args, **kwargs)
+        return tf.linalg.LinearOperatorFullMatrix(
+            operator_matrix, is_square=True, name=name)
+    return wrapper
+
+
+### Constant operators
+
+@linear_operator
+def sigma_x():
+    return tf.constant([[0., 1.], [1., 0.]], dtype=c64)
+
+
+@linear_operator
+def sigma_y():
+    return tf.constant([[0.j, -1.j], [1.j, 0.j]], dtype=c64)
+
+
+@linear_operator
+def sigma_z():
+    return tf.constant([[1., 0.], [0., -1.]], dtype=c64)
+
+
+@linear_operator
+def identity(N):
     """Returns an identity operator in the Fock basis.
 
     Args:
         N (int): Dimension of Hilbert space
-        dtype (tf.dtypes.DType, optional): Returned dtype. Defaults to c64.
 
     Returns:
-        Tensor([N, N], dtype): NxN identity operator
+        Tensor([N, N], tf.complex64): NxN identity operator
     """
-    return tf.eye(N, dtype=dtype)
+    return tf.eye(N, dtype=c64)
 
 
-def destroy(N, dtype=tf.complex64):
+@linear_operator
+def destroy(N):
     """Returns a destruction (lowering) operator in the Fock basis.
 
     Args:
         N (int): Dimension of Hilbert space
-        dtype (tf.dtypes.DType, optional): Returned dtype. Defaults to c64.
 
     Returns:
-        Tensor([N, N], dtype): NxN creation operator
+        Tensor([N, N], tf.complex64): NxN creation operator
     """
-    a = diag(tf.sqrt(tf.range(1, N, dtype=tf.float64)), k=1)
-    return tf.cast(a, dtype=dtype)
+    a = diag(tf.sqrt(tf.range(1, N, dtype=tf.float32)), k=1)
+    return tf.cast(a, dtype=c64)
 
 
-def create(N, dtype=tf.complex64):
+@linear_operator
+def create(N):
     """Returns a creation (raising) operator in the Fock basis.
 
     Args:
         N (int): Dimension of Hilbert space
-        dtype (tf.dtypes.DType, optional): Returned dtype. Defaults to c64.
 
     Returns:
-        Tensor([N, N], dtype): NxN creation operator
+        Tensor([N, N], tf.complex64): NxN creation operator
     """
-    # Preserve max precision in intermediate calculations until final cast
-    return tf.cast(tf.linalg.adjoint(destroy(N, dtype=tf.complex128)), dtype=dtype)
+    return destroy(N).adjoint().to_dense()
 
 
-def num(N, dtype=tf.complex64):
+@linear_operator
+def num(N):
     """Returns the number operator in the Fock basis.
 
     Args:
         N (int): Dimension of Hilbert space
-        dtype (tf.dtypes.DType, optional): Returned dtype. Defaults to c64.
 
     Returns:
-        Tensor([N, N], dtype): NxN number operator
+        Tensor([N, N], tf.complex64): NxN number operator
     """
-    return tf.cast(diag(tf.range(0, N)), dtype=dtype)
+    return tf.cast(diag(tf.range(0, N)), dtype=c64)
 
 
-def position(N, dtype=tf.complex64):
+@linear_operator
+def position(N):
     """Returns the position operator in the Fock basis.
 
     Args:
         N (int): Dimension of Hilbert space
-        dtype (tf.dtypes.DType, optional): Returned dtype. Defaults to c64.
 
     Returns:
-        Tensor([N, N], dtype): NxN position operator
+        Tensor([N, N], tf.complex64): NxN position operator
     """
     # Preserve max precision in intermediate calculations until final cast
-    sqrt2 = tf.sqrt(tf.constant(2, dtype=tf.complex128))
-    a_dag = create(N, dtype=tf.complex128)
-    a = destroy(N, dtype=tf.complex128)
-    return tf.cast((a_dag + a) / sqrt2, dtype=dtype)
+    sqrt2 = tf.sqrt(tf.constant(2, dtype=c64))
+    a_dag = create(N).to_dense()
+    a = destroy(N).to_dense()
+    return tf.cast((a_dag + a) / sqrt2, dtype=c64)
 
 
-def momentum(N, dtype=tf.complex64):
+@linear_operator
+def momentum(N):
     """Returns the momentum operator in the Fock basis.
 
     Args:
         N (int): Dimension of Hilbert space
 
     Returns:
-        Tensor([N, N], c64): NxN momentum operator
+        Tensor([N, N], tf.complex64): NxN momentum operator
     """
     # Preserve max precision in intermediate calculations until final cast
-    sqrt2 = tf.sqrt(tf.constant(2, dtype=tf.complex128))
-    a_dag = create(N, dtype=tf.complex128)
-    a = destroy(N, dtype=tf.complex128)
-    return tf.cast(1j * (a_dag - a) / sqrt2, dtype=dtype)
+    sqrt2 = tf.sqrt(tf.constant(2, dtype=c64))
+    a_dag = create(N).to_dense()
+    a = destroy(N).to_dense()
+    return tf.cast(1j * (a_dag - a) / sqrt2, dtype=c64)
 
 
-def parity(N, dtype=tf.complex64):
+@linear_operator
+def parity(N):
     """Returns the photon number parity operator in the Fock basis.
 
     Args:
         N (int): Dimension of Hilbert space
-        dtype (tf.dtypes.DType, optional): Returned dtype. Defaults to c64.
 
     Returns:
-        Tensor([N, N], dtype): NxN photon number parity operator
+        Tensor([N, N], tf.complex64): NxN photon number parity operator
     """
-    diag = tf.where(tf.math.floormod(tf.range(N),2)==1, -1, 1)
-    return tf.linalg.diag(tf.cast(diag, tf.complex64))
+    pm1 = tf.where(tf.math.floormod(tf.range(N),2)==1, -1, 1)
+    return diag(tf.cast(pm1, dtype=c64))
+
+@linear_operator
+def projector(n, N):
+    """
+    Returns a projector onto n-th basis state in N-dimensional Hilbert space.
+
+    Args:
+        n (int): index of basis vector
+        N (int): Dimension of Hilbert space
+
+    Returns:
+        Tensor([N, N], tf.complex64): NxN photon number parity operator
+    """
+    assert n < N
+    return diag(tf.one_hot(n, N, dtype=c64))
+
+
+### Parametrized operators
+
+class ParametrizedOperator():
+    
+    def __init__(self, N, name=None):
+        """
+        Args:
+            N (int): dimension of Hilbert space
+            name (str, optional): name of LinearOperator instance
+
+        """
+        self.N = N
+        self.name = name if name else self.__class__.__name__
+
+    def __call__(self, *args, **kwargs):
+        kwargs['name'] = self.name
+        return self.compute(*args, **kwargs)
+
+    @linear_operator
+    @tf.function  
+    def compute(self, *args, **kwargs):
+        return self._compute(*args, **kwargs)
+    
+    def _compute(self):
+        """Subclasses need to implement this."""
+
+
+class TranslationOperator(ParametrizedOperator):
+    """ 
+    Translation in phase space.
+    
+    Example:
+        T = TranslationOperator(100)
+        alpha = tf.constant([1.23+0.j, 3.56j, 2.12+1.2j])
+        T(alpha) # shape=[3,100,100]
+        state = T(alpha).matvec(state)
+    """
+    
+    def __init__(self, N):
+        """ Pre-diagonalize position and momentum operators."""
+        p = momentum(N).to_dense()
+        q = position(N).to_dense()
+        
+        # Pre-diagonalize
+        (self._eig_q, self._U_q) = tf.linalg.eigh(q)
+        (self._eig_p, self._U_p) = tf.linalg.eigh(p)
+        self._qp_comm = tf.linalg.diag_part(q @ p - p @ q)
+        super().__init__(N=N)
+
+    def _compute(self, amplitude, *args, **kwargs):
+        """Calculates T(amplitude) for a batch of amplitudes using BCH.
+
+        Args:
+            amplitude (Tensor([B1, ..., Bb], c64)): A batch of amplitudes
+
+        Returns:
+            Tensor([B1, ..., Bb, N, N], c64): A batch of T(amplitude)
+        """
+        # Reshape amplitude for broadcast against diagonals
+        amplitude = tf.cast(tf.expand_dims(amplitude, -1), dtype=c64)
+
+        # Take real/imag of amplitude for the commutator part of the expansion
+        re_a = tf.cast(tf.math.real(amplitude), dtype=c64)
+        im_a = tf.cast(tf.math.imag(amplitude), dtype=c64)
+
+        # Exponentiate diagonal matrices
+        expm_q = tf.linalg.diag(tf.math.exp(1j * im_a * self._eig_q))
+        expm_p = tf.linalg.diag(tf.math.exp(-1j * re_a * self._eig_p))
+        expm_c = tf.linalg.diag(tf.math.exp(-0.5 * re_a * im_a * self._qp_comm))
+
+        # Apply Baker-Campbell-Hausdorff
+        return tf.cast(
+            self._U_q
+            @ expm_q
+            @ tf.linalg.adjoint(self._U_q)
+            @ self._U_p
+            @ expm_p
+            @ tf.linalg.adjoint(self._U_p)
+            @ expm_c,
+            dtype=c64,
+        )
+
+
+class DisplacementOperator(TranslationOperator):
+    """ 
+    Displacement in phase space D(amplitude) = T(amplitude * sqrt(2)).
+    
+    """    
+    def __call__(self, amplitude):
+        sqrt2 = tf.math.sqrt(tf.constant(2, dtype=amplitude.dtype))
+        return super().__call__(amplitude*sqrt2)
+
+
+class RotationOperator(ParametrizedOperator):
+    """ Rotation in phase space."""    
+
+    def _compute(self, phase, *args, **kwargs):
+        """Calculates R(phase) = e^{-i*phase*n} for a batch of phases.
+
+        Args:
+            phase (Tensor([B1, ..., Bb], c64)): A batch of phases
+
+        Returns:
+            Tensor([B1, ..., Bb, N, N], c64): A batch of R(phase)
+        """
+        phase = tf.cast(tf.expand_dims(phase, -1), dtype=c64)
+        exp_diag = tf.math.exp(phase * tf.cast(tf.range(self.N), c64))
+        return tf.linalg.diag(exp_diag)
+
+
+class SNAP(ParametrizedOperator):
+    """
+    Selective Number-dependent Arbitrary Phase (SNAP) gate.
+    SNAP(theta) = sum_n( e^(i*theta_n) * |n><n| )
+    
+    """          
+    def _compute(self, theta, *args, **kwargs):
+        """Calculates ideal SNAP(theta) for a batch of SNAP parameters.
+
+        Args:
+            theta (Tensor([B1, ..., Bb, S], c64)): A batch of parameters.
+
+        Returns:
+            Tensor([B1, ..., Bb, N, N], c64): A batch of SNAP(theta)
+        """
+        S = theta.shape[-1] # SNAP truncation
+        D = len(theta.shape)-1
+        paddings = tf.constant([[0,0]]*D + [[0,self.N-S]])
+        theta = tf.cast(theta, dtype=c64)
+        theta = tf.pad(theta, paddings)
+        exp_diag = tf.math.exp(1j*theta)
+        return tf.linalg.diag(exp_diag)
+    
+
+class QubitRotationXY(ParametrizedOperator):
+    """
+    Qubit rotation in xy plane.
+    R(angle, phase) = e^(-i*angle/2*[cos(phase)*sx + sin(phase*sy]))
+    
+    """
+    def __init__(self):
+        super().__init__(N=2)
+
+    def _compute(self, angle, phase, *args, **kwargs):
+        """Calculates rotation matrix for a batch of rotation angles.
+
+        Args:
+            angle (Tensor([B1, ..., Bb], float32)): batched angle of rotation
+                in radians, i.e. angle=pi corresponds to full qubit flip.
+            phase (Tensor([B1, ..., Bb], float32)): batched axis of rotation
+                in radians, where by convention 0 is x axis.
+
+        Returns:
+            Tensor([B1, ..., Bb, 2, 2], c64): A batch of R(angle, phase)
+        """
+        assert angle.shape == phase.shape
+        angle = tf.cast(tf.reshape(angle, angle.shape+[1,1]), c64)
+        phase = tf.cast(tf.reshape(phase, phase.shape+[1,1]), c64)
+        
+        sx = sigma_x().to_dense()
+        sy = sigma_y().to_dense()
+        I = identity(2).to_dense()
+        
+        R = tf.math.cos(angle/2) * I - 1j*tf.math.sin(angle/2) * \
+            (tf.math.cos(phase)*sx + tf.math.sin(phase)*sy)
+        return R
+
+
+class Phase(ParametrizedOperator):
+    """ Simple phase factor."""
+   
+    def _compute(self, angle, *args, **kwargs):
+        """
+        Calculates batch phase factor e^(i*angle)
+
+        Args:
+            angle (Tensor([B1, ..., Bb], float32)): batch of angles in radians
+            
+        Returns:
+            Tensor([B1, ..., Bb, N, N], c64): A batch of phase factors
+        """
+        angle = tf.cast(tf.reshape(angle, angle.shape+[1,1]), c64)
+        return tf.math.exp(1j*angle) * tf.eye(self.N, dtype=c64)
+
--- simulator/utils.py
+++ simulator/utils.py
@@ -1,92 +1,151 @@
+# -*- coding: utf-8 -*-
 """
-Simulator helper functions
+Created on Sat Nov 28 11:45:55 2020
 
-Created on Sun Jul 26 20:55:36 2020
-
-@author: Henry Liu
+@author: Vladimir Sivak
 """
+
 import tensorflow as tf
-from tensorflow.keras.backend import batch_dot
+from tensorflow import complex64 as c64
 
 
-def matrix_flatten(tensor):
-    """Takes a tensor of arbitrary shape and returns a "flattened" vector of matrices.
+def normalize(state):
+    """
+    Args:
+        state (Tensor([B1,...Bb,N], c64)): batch of quantum states.
+    
+    Returns:
+        state_normalized (Tensor([B1,...Bb,N], c64)): normalized quantum state
+        norm (Tensor([B1,...Bb,1], c64)): norm of the batch of states
+    """
+    return tf.linalg.normalize(state, axis=-1)
 
-    This is useful to get the correct broadcasting shape for batch operations.
+def basis(n, N, batch_shape=[1]):
+    """
+    Args:
+        n (int): index of basis vector
+        N (int): Dimension of Hilbert space 
+        batch_shape (optional): shape for batch of identical vectors.
+        
+    Examples:
+        basis(2,100,batch_shape=[]) -- unbatched oscillator Fock=2 state.
+        basis(0,2,batch_shape=[10]) -- batch of 10 qubit 'g' states.
+    """
+    return tf.ones(batch_shape+[1], c64) * tf.one_hot(n, N, dtype=c64)
+
+def Kronecker_product(states):
+    """
+    Kronecker product of states living in different Hilbert spaces.
+    
+    Args:
+        states: [Tensor([B1,...Bb,N1], c64), ..., Tensor([B1,...Bb,Nk], c64)]
+            list of k batched states. All states should have the same batch
+            shape, and can live in different Hilbert spaces with dimensions
+            N1...Nk. The final tensor-product state will have the same batch 
+            shape and Hilbert space dimension prod_k(Nk).
+            
+    Returns:
+        tensor_state (Tensor([B1,...Bb,N1*...*Nk], c64)): tensor-product state
+        
+    Example:
+        g = basis(0,2,[30])   # batch of 30 qubit ground states
+        fock5 = basis(5,100,[30])  # batch of 30 oscillator Fock=5 states
+        psi = Kronecker_product([g, fock5]) # state in combined Hilbert space
+    """
+    # expand dims to trick the tf.linalg.LinearOperatorKronecker
+    states = [tf.expand_dims(s,-1) for s in states]
+    operators = list(map(tf.linalg.LinearOperatorFullMatrix, states))
+    tensor_state = tf.linalg.LinearOperatorKronecker(operators).to_dense()
+    return tf.squeeze(tensor_state, axis=-1)
+    
+import tensorflow_probability as tfp
+
+@tf.function
+def measurement(state, M_ops, sample=True):
+    """
+    Batch measurement projection.
 
     Args:
-        tensor (Tensor([x, y, ...])): A tensor with arbitrary shape
+        state (Tensor([B1,...Bb,N], c64)): batch of quantum states
+        M_ops (dict, LinearOperator): dictionary of measurement operators 
+            corresponding to two different qubit measurement outcomes.
+        sample (bool): flag to sample or return expectation value
+
+    Output:
+        state (Tensor([B1,...Bb,N], c64)): batch of collapsed quantum states if
+            sample=true, otherwise same as input state.
+        obs (Tensor([B1,...Bb], float32)): measurement outcomes from {-1,1} if
+            sample=true, otherwise expectation value of qubit sigma_z.
+
+    """
+    collapsed, p = {}, {}
+    state, _ = normalize(state)
+    
+    for i in M_ops.keys():
+        collapsed[i] = M_ops[i].matvec(state)
+        collapsed[i], p[i] = normalize(collapsed[i])
+        p[i] = tf.squeeze(tf.math.real(p[i]), axis=-1)
+
+
+    if sample:
+        obs = tfp.distributions.Bernoulli(probs=p[1]/(p[0]+p[1])).sample()
+        state = tf.where(obs==1, collapsed[1], collapsed[0])
+        obs = 1 - 2 * obs  # convert to {-1,1}
+        obs = tf.cast(obs, dtype=tf.float32)
+        return state, obs
+    else:
+        return state, (p[0] - p[1])/(p[0]+p[1])
+
+@tf.function
+def batch_dot(state1, state2):
+    """
+    Batch dot-product of two states: conj(state1) * state2
+
+    Args:
+        state1, state2 (Tensor([B1,...Bb,N], c64)): batches of states. If one
+            of them has batch_shape = [1] or [], will do broadcasting.
 
     Returns:
-        Tensor([numx * numy * ..., 1, 1]): A "flattened" vector of matrices
+        dot_product (Tensor([B1,...Bb,1], c64))
     """
-    tensor = tf.reshape(tensor, [-1])
-    tensor = tf.reshape(tensor, shape=[tensor.shape[0], 1, 1])
-    return tensor
+    return tf.math.reduce_sum(tf.math.conj(state1) * state2, axis=-1, keepdims=True)
 
-
-def normalize(state, dtype=tf.complex64):
+@tf.function
+def expectation(state, operator, reduce_batch=True):
     """
-    Batch normalization of the wave function.
-
-    Input:
-        state -- batch of state vectors; shape=[batch_size,NH]
-
-    """
-    norm = tf.math.real(batch_dot(tf.math.conj(state), state))
-    norm = tf.cast(tf.math.sqrt(norm), dtype=dtype)
-    state = state / norm
-    return state
-
-
-def expectation(psi, Op, reduce_batch=True):
-    """
-    Expectation of operator 'Op' in state 'psi'. 
+    Expectation value of <operator> in <state>.
+    
+    Args:
+        state (Tensor([B1,...Bb,N], c64)): batch of states
+        operator (LinearOperator): operator; can be batched.
+        reduce_batch (bool): flag to average result over the batch.
+        
     Supports various batching options:
         
-        1) psi.shape = [batch_size,N]
-           Op.shape  = [batch_size,N,N]
+        1) state.shape = [B1,...,Bb,N]
+           operator.shape  = [B1,...,Bb,N,N]
            
-           Returns a batch of expectation values of shape=[batch_size,1] 
+           Returns a batch of expectation values of shape=[B1,...,Bb,1] 
         
-        2) psi.shape = [1,N]
-           Op.shape  = [batch_size,N,N]
+        2) shape.shape = [1,N] or [N]
+           operator.shape  = [B1,...,Bb,N,N]
            
-           Broadcasts 'psi' and returns a batch of expectation values of 
-           shape=[batch_size,1] 
+           Broadcasts 'state' and returns a batch of expectation values of 
+           shape=[batch_size,1]
 
-        3) psi.shape = [batch_size,N]
-           Op.shape  = [N,N]
+        3) state.shape = [B1,...,Bb,N]
+           operator.shape  = [N,N]
            
-           If reduce_batch=False, returns a batch of expectation values of 
-           shape=[batch_size,1]. If reduce_batch=True, reduces over a batch 
+           If reduce_batch=False returns a batch of expectation values of 
+           shape=[B1,...,Bb,1]. If reduce_batch=True reduces over a batch 
            of states and returns a single expectation value of shape [].
 
     """
-    psi = normalize(psi)
-    
-    # batched operator, batched or single wavefunction
-    if len(Op.shape) == 3:
-        assert Op.shape[1] == Op.shape[2] # N
-        assert Op.shape[1] == psi.shape[1] # N
-        
-        # if single wavefunction, broadcase to batched
-        if psi.shape[0] == 1:
-            psi = tf.broadcast_to(psi, [Op.shape[0],psi.shape[1]])
-    
-        assert Op.shape[0] == psi.shape[0] # batch_size
-        
-        expect_batch = batch_dot(tf.math.conj(psi), batch_dot(Op, psi))
-        
-    # single operator, batched wavefunction
-    if len(Op.shape) == 2:
-        assert Op.shape[0] == Op.shape[1] # N
-        assert Op.shape[0] == psi.shape[1] # N
+    state, _ = normalize(state)
+    expect_batch = batch_dot(state, operator.matvec(state))
 
-        expect_batch = batch_dot(tf.math.conj(psi), tf.linalg.matvec(Op, psi))
-
-        if reduce_batch: 
-            expect_batch_reduced = tf.math.reduce_mean(expect_batch)
-            return expect_batch_reduced
+    if reduce_batch: 
+        expect_batch_reduced = tf.math.reduce_mean(expect_batch)
+        return expect_batch_reduced
 
     return expect_batch
