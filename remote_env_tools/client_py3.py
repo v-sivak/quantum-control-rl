a935dedee64f0034700fa67fbc0b246017dddaab
Vladimir Sivak <sivak.volodymyr@gmail.com>
2021-01-03 15:37:07 -0500

    Convert sockets to classes

--- remote_env_tools/client_py3.py
+++ remote_env_tools/client_py3.py
@@ -9,26 +9,24 @@ Created on Fri Jan  1 19:22:28 2021
 import pickle
 import socket
 import time
-from server import recv_data, send_data, HEADERSIZE
+from server import Client
 
 def test_create_data(i):
     time.sleep(3)
     return {'R' : [i,i+1]}
 
-def client(N=10, create_data=test_create_data):
-    host = socket.gethostname()
-    port = 5000
-    client_socket = socket.socket()
-    client_socket.connect((host, port))
-    for i in range(N):
-        # receive action data from agent
-        data = recv_data(client_socket)
-        # send reward data to agent
-        data = create_data(i)
-        send_data(data, client_socket)
-    client_socket.close()
-
 
 if __name__ == '__main__':
-    client()
+    
+    client_socket = Client()
+    (host, port) = socket.gethostname(), 5000
+    client_socket.connect((host, port))
 
+    for i in range(10):
+        # receive action data from agent
+        data = client_socket.recv_data()
+        # send reward data to agent
+        data = test_create_data(i)
+        client_socket.send_data(data)
+    
+    client_socket.close()
--- remote_env_tools/server.py
+++ remote_env_tools/server.py
@@ -11,53 +11,86 @@ import socket
 import logging
 logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S')
 
-HEADERSIZE = 10
-pickle_protocol = 2
+class PickleSocket(socket.socket):
+    """ This is a simple python socket to send pickled data over TCP/IP."""
+    pickle_protocol = 2
+    HEADERSIZE = 10
+    
+    def recv_data(self, connection):
+        full_msg = b''
+        new_msg = True
+        msg_ended = False
+        while not msg_ended:
+            msg = connection.recv(16)
+            if new_msg:
+                msglen = int(msg[:self.HEADERSIZE])
+                logging.info('New msg len: %d' % msglen)
+                new_msg = False
+            full_msg += msg
+            if len(full_msg) - self.HEADERSIZE == msglen:
+                logging.info('Full msg recieved')
+                data = pickle.loads(full_msg[self.HEADERSIZE:])
+                print(data)
+                msg_ended = True
+        return data
 
-def recv_data(connection):
-    full_msg = b''
-    new_msg = True
-    msg_ended = False
-    while not msg_ended:
-        msg = connection.recv(16)
-        if new_msg:
-            msglen = int(msg[:HEADERSIZE])
-            logging.info('New msg len: %d' % msglen)
-            new_msg = False
-        full_msg += msg
-        if len(full_msg) - HEADERSIZE == msglen:
-            logging.info('Full msg recieved')
-            data = pickle.loads(full_msg[HEADERSIZE:])
-            print(data)
-            msg_ended = True
-    return data
+    def send_data(self, data, connection):
+        msg = pickle.dumps(data, protocol=self.pickle_protocol)
+        msg = bytes(str(len(msg)).zfill(self.HEADERSIZE), 'utf-8') + msg
+        connection.send(msg)
 
-def send_data(data, connection):
-    msg = pickle.dumps(data, protocol=pickle_protocol)
-    msg = bytes(str(len(msg)).zfill(HEADERSIZE), 'utf-8') + msg
-    # msg = bytes(f'{len(msg):<{HEADERSIZE}}', 'utf-8') + msg
-    connection.send(msg)
+    
+class Server(PickleSocket):
+    """ A server for a single client. 
+    
+    Intended use: server is on the RL agent side, client is on the environment 
+    side. Server sends actions to client, client sends rewards back to server.
+    """
+    def send_data(self, data):
+        super().send_data(data, self.client_socket)
+    
+    def recv_data(self):
+        return super().recv_data(self.client_socket)
+    
+    def connect_client(self):
+        self.listen(1)
+        self.client_socket, self.client_address = server_socket.accept()
+        logging.warning('Connection with: ' + str(self.client_address))
+
+    def disconnect_client(self):
+        self.client_socket.close()
+
+
+class Client(PickleSocket):
+    """ A simple client. 
+    
+    Intended use: server is on the RL agent side, client is on the environment 
+    side. Server sends actions to client, client sends rewards back to server.
+    """
+    def send_data(self, data):
+        super().send_data(data, self)
+    
+    def recv_data(self):
+        return super().recv_data(self)
+
 
 def test_create_data(i):
     return {'alpha': [i**2, i**3], 'beta': [i**4,i+1]}
 
 
-def server(N=10, create_data=test_create_data):
-    host = socket.gethostname()
-    port = 5000
-    server_socket = socket.socket()
-    server_socket.bind((host, port))
-    server_socket.listen(2)
-    client_socket, address = server_socket.accept()
-    logging.warning('Connection with: ' + str(address))
-    for i in range(N):
-        # send action data to fpga
-        data = create_data(i)
-        send_data(data, client_socket)
-        # receive reward data from fpga
-        data = recv_data(client_socket)
-    client_socket.close()
-
 
 if __name__ == '__main__':
-    server()
+    
+    server_socket = Server()
+    (host, port) = socket.gethostname(), 5000
+    server_socket.bind((host, port))
+    server_socket.connect_client()
+    
+    for i in range(10):
+        # send action data to fpga
+        data = test_create_data(i)
+        server_socket.send_data(data)
+        # receive reward data from fpga
+        data = server_socket.recv_data()
+    
+    server_socket.disconnect_client()
