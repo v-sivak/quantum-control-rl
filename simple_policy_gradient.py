ec5ebf57af465a8feac1dc244b57335d2e647f8b
Vladimir Sivak <sivak.volodymyr@gmail.com>
2021-01-03 14:36:53 -0500

    Significant improvements of PPO for single-state envs
    * Added multiple policy updates per epoch
    * Added gradient and log_prob clipping to stabilize training

--- simple_policy_gradient.py
+++ simple_policy_gradient.py
@@ -3,29 +3,40 @@
 Created on Tue Dec 29 18:16:23 2020
 
 @author: Vladimir Sivak
+
+This is a straightforward implementation of PPO http://arxiv.org/abs/1707.06347
+for simple single-state RL environments.
 """
 import tensorflow as tf
 import tensorflow_probability as tfp
 
-actions = ['alpha']#, 'phi_e', 'phi_g']
+actions = ['alpha', 'phi_e', 'phi_g']
 
+from untitled0 import reward_sampler, test_eval
 
 # trainable variables
-mean = {s : tf.Variable(tf.random.uniform([]), name='mean_'+s) for s in actions}
+mean = {s : tf.Variable(tf.random.normal([], stddev=0.05), name='mean_'+s) for s in actions}
 sigma = {s : tf.Variable(0.5, name='sigma_'+s) for s in actions}
 baseline = tf.Variable(0.0, name='baseline')
 
 
+# TODO: initialization should be more narrow, copy from tf-agents
+# FIgure out the correct value loss coeff here
+
 algo = 'PPO'
 B = 200 # batch_size
-EPOCHS = 1000
-lr = 1e-4
+EPOCHS = 3000
+lr = 1e-3
 policy_steps = 20
 
-def reward_sampler(a):
-    p = 1/(1+500*(a['alpha'] - 0.37)**2) # just needed a sharper reward funciton!
-    z = tfp.distributions.Bernoulli(probs=p).sample()
-    return 2*tf.cast(z, tf.float32)-1
+log_prob_clipping = 10.
+gradient_clipping = 1.
+importance_ratio_eps = 0.2
+
+# def reward_sampler(a):
+#     p = 1/(1+500*(a['alpha'] - 0.37)**2) # just needed a sharper reward funciton!
+#     z = tfp.distributions.Bernoulli(probs=p).sample()
+#     return 2*tf.cast(z, tf.float32)-1
 
 
 def compute_log_prob(action, mean, sigma):
@@ -48,25 +59,26 @@ for epoch in range(EPOCHS):
     
     # log prob according to old policy (required for importance ratio)
     if epoch == 0: mean_old, sigma_old = mean, sigma 
-    log_prob_old = compute_log_prob(a, mean_old, sigma_old)         
+    log_prob_old = compute_log_prob(a, mean_old, sigma_old)
+    log_prob_old = tf.clip_by_value(log_prob_old, -log_prob_clipping, log_prob_clipping)
     mean_old = tf.nest.map_structure(tf.identity, mean)
     sigma_old = tf.nest.map_structure(tf.identity, sigma)
     
+    # calculate policy loss and do several gradient updates
     for i in range(policy_steps):
-        # calculate policy loss and do gradient update
-        with tf.GradientTape() as tape:    
+        with tf.GradientTape() as tape:
             # log prob according to the current policy
             log_prob = compute_log_prob(a, mean, sigma)
-    
+            log_prob = tf.clip_by_value(log_prob, -log_prob_clipping, log_prob_clipping)
+            
             A = R - baseline # action advantages       
     
             if algo == 'REINFORCE':
                 policy_loss_batch = - A * log_prob
-    
+                
             if algo == 'PPO':
-                eps = 0.2
                 importance_ratio = tf.math.exp(log_prob - log_prob_old)
-                importance_ratio_clip = tf.clip_by_value(importance_ratio, 1-eps, 1+eps)
+                importance_ratio_clip = tf.clip_by_value(importance_ratio, 1-importance_ratio_eps, 1+importance_ratio_eps)
                 policy_loss_batch = - tf.minimum(importance_ratio*A, importance_ratio_clip*A)
             
             policy_loss = tf.reduce_mean(policy_loss_batch) # reduce over batch
@@ -74,10 +86,13 @@ for epoch in range(EPOCHS):
             loss = policy_loss + value_loss
             
             grads = tape.gradient(loss, tape.watched_variables())
+            grads = tf.clip_by_value(grads, -gradient_clipping, gradient_clipping)
             optimizer.apply_gradients(zip(grads, tape.watched_variables()))
             
     eval_action = tf.nest.map_structure(tf.math.tanh, mean)
-    print(eval_action['alpha'])
+    if epoch % 10 == 0: 
+        print(test_eval(eval_action))
+    # print(eval_action['alpha'])
         
 
 
