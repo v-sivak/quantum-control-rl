8572fa0d403058b95f0aeb70e7148a552dba260c
Vladimir Sivak <vladimir.sivak@yale.edu>
2021-01-26 10:42:59 -0500

    Folder with Murch cooling protocol experiments

--- fpga_exp/qubit_cooling_Murch/qubit_cooling_rotation_sweep.py
+++ fpga_exp/qubit_cooling_Murch/qubit_cooling_rotation_sweep.py
@@ -1,28 +1,39 @@
 # -*- coding: utf-8 -*-
 """
+Created on Mon Jan 25 11:10:10 2021
+
+@author: qulab
+"""
+
+
+# -*- coding: utf-8 -*-
+"""
 Created on Sun Jan 24 13:43:48 2021
 
 @author: qulab
 """
 from init_script import *
 
-class qubit_cooling_time_sweep(FPGAExperiment):
+class qubit_cooling_rotation_sweep(FPGAExperiment):
     readout_detune_MHz = FloatParameter(50.4)
     qubit_detune_MHz = FloatParameter(8.0)
     readout_amp = FloatParameter(0.22)
     qubit_amp = FloatParameter(0.2)
     wait_time = IntParameter(100)
-    time_range = RangeParameter((0,1000,21))
     loop_delay = IntParameter(500e3)
     flip_qubit = BoolParameter(False)
+    tau = IntParameter(1000)
+    phase_range = RangeParameter((-2., 2., 201))
+    
+    fit_func = 'gaussian'
 
        
     def sequence(self):        
         ssb0 = -50e6
         ssb_q = ssb0 + self.qubit_detune_MHz*1e6
         ssb_r = ssb0 + self.readout_detune_MHz*1e6
-
-        for tau in np.linspace(*self.time_range):
+        
+        with scan_phase(qubit.chan, 0.5*qubit.pulse.unit_amp, *self.phase_range):
             sync()
             qubit.set_ssb(ssb0)
             delay(24)
@@ -32,15 +43,14 @@ class qubit_cooling_time_sweep(FPGAExperiment):
             sync()
             if self.flip_qubit:
                 qubit.flip()
-            sync()
+                sync()
             qubit.set_ssb(ssb_q)
             delay(24)
             sync()
-            duration = 4 * (tau.astype(int) // 4)
-            readout_aux.constant_pulse(duration, amp=self.readout_amp)
-            qubit.constant_pulse(duration, amp=self.qubit_amp)
+            readout_aux.constant_pulse(self.tau, amp=self.readout_amp)
+            qubit.constant_pulse(self.tau, amp=self.qubit_amp)
             sync()
-            qubit.pi2_pulse(phase=-np.pi/2)
+            qubit.pulse(amp='dynamic')
             sync()
             delay(self.wait_time)
             sync()
---
+++ fpga_exp/qubit_cooling_Murch/qubit_cooling_time_sweep.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 24 13:43:48 2021
+
+@author: qulab
+"""
+from init_script import *
+
+class qubit_cooling_time_sweep(FPGAExperiment):
+    readout_detune_MHz = FloatParameter(50.4)
+    qubit_detune_MHz = FloatParameter(8.0)
+    readout_amp = FloatParameter(0.22)
+    qubit_amp = FloatParameter(0.2)
+    wait_time = IntParameter(100)
+    time_range = RangeParameter((0,1000,21))
+    loop_delay = IntParameter(500e3)
+    flip_qubit = BoolParameter(False)
+    phase_coeff = FloatParameter(0.5)
+       
+    def sequence(self):        
+        ssb0 = -50e6
+        ssb_q = ssb0 + self.qubit_detune_MHz*1e6
+        ssb_r = ssb0 + self.readout_detune_MHz*1e6
+
+        for tau in np.linspace(*self.time_range):
+            sync()
+            qubit.set_ssb(ssb0)
+            delay(24)
+            sync()
+            readout_aux.set_ssb(ssb_r)
+            delay(24)
+            sync()
+            if self.flip_qubit:
+                qubit.flip()
+            sync()
+            qubit.set_ssb(ssb_q)
+            delay(24)
+            sync()
+            duration = 4 * (tau.astype(int) // 4)
+            readout_aux.constant_pulse(duration, amp=self.readout_amp)
+            qubit.constant_pulse(duration, amp=self.qubit_amp)
+            sync()
+            qubit.rotate(np.pi/2, phase=-self.phase_coeff*np.pi)
+            sync()
+            delay(self.wait_time)
+            sync()
+            readout()
+            sync()
+            delay(self.loop_delay)
+            
