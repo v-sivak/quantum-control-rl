dc6fc7227d7eab54889adacd31af7b662ce26147
Vladimir Sivak <vladimir.sivak@yale.edu>
2021-02-08 20:07:09 -0500

    some more Wigner testing scripts

--- CD_gate/CD_cat_wigner.py
+++ CD_gate/CD_cat_wigner.py
@@ -17,6 +17,8 @@ class CD_cat_wigner(FPGAExperiment):
         cavity_pulse, qubit_pulse = C.make_pulse(self.tau, self.alpha, 0., 0.)
 
         with system.wigner_tomography(*self.disp_range):
+            readout(init='se')
+            sync()
             qubit.pi2_pulse(phase=np.pi/2)
             sync()
             cavity.array_pulse(*cavity_pulse)
@@ -25,13 +27,14 @@ class CD_cat_wigner(FPGAExperiment):
             qubit.pi2_pulse(phase=np.pi/2)
             sync()
             readout(msmt='se')
-            qubit.flip()
-            delay(24+252+24+1000+12)
 
 
     def process_data(self):
+        init = self.results['init'].threshold()
         msmt = self.results['msmt'].threshold()
         for i in [0,1]:
-            self.results['postselected'+str(i)] = self.results['default'].postselect(msmt, [i])[0].thresh_mean().data
-            self.results['postselected'+str(i)].ax_data = self.results['msmt'].ax_data[1:]
-            self.results['postselected'+str(i)].labels = self.results['msmt'].labels[1:]
+            for j in [0,1]:
+                result = self.results['default'].postselect(init, [i])[0].postselect(msmt, [j])[0]
+                self.results['postselected'+str(i)+str(j)] = result.thresh_mean().data
+                self.results['postselected'+str(i)+str(j)].ax_data = self.results['msmt'].ax_data[1:]
+                self.results['postselected'+str(i)+str(j)].labels = self.results['msmt'].labels[1:]
---
+++ CD_gate/CD_ge_wigner.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 31 13:56:43 2021
+
+@author: qulab
+"""
+from init_script import *
+from conditional_displacement_compiler import ConditionalDisplacementCompiler
+from scipy.optimize import curve_fit
+import numpy as np
+
+
+class CD_ge_wigner(FPGAExperiment):
+    disp_range = RangeParameter((-4.0, 4.0, 101))
+    tau = IntParameter(90)
+    alpha = FloatParameter(10.0)
+    flip_qubit = BoolParameter(False)
+
+    def sequence(self):
+        C = ConditionalDisplacementCompiler()
+        cavity_pulse, qubit_pulse = C.make_pulse(self.tau, self.alpha, 0., 0.)
+
+        with system.wigner_tomography(*self.disp_range):
+            sync()
+            if self.flip_qubit:
+                qubit.flip()
+                sync()
+            cavity.array_pulse(*cavity_pulse)
+            qubit.array_pulse(*qubit_pulse)
+            sync()
+            if not self.flip_qubit:
+                qubit.flip()
+                sync()
+
+    def plot(self, fig, data):
+        # create phase space grid
+        points = self.disp_range[-1]
+        axis = np.linspace(*self.disp_range)
+        x, y = np.meshgrid(axis, axis)
+
+        # fit the blob to 2D Gaussian
+        sigmaz = 1 - 2*self.results['default'].threshold()
+        mean_sigmaz = sigmaz.mean(axis=0).data
+
+        ind = np.unravel_index(np.argmax(mean_sigmaz, axis=None), mean_sigmaz.shape)
+
+        data = mean_sigmaz.ravel()
+        initial_guess = (0.7, axis[ind[1]], axis[ind[0]], 0.5, 0.5, 0, 0)
+        popt, pcov = curve_fit(gaussian2D, (x, y), data, p0=initial_guess)
+        data_fitted = gaussian2D((x, y), *popt)
+        self.popt = popt
+
+        # plot data and fit
+        ax = fig.add_subplot(111)
+        im = ax.imshow(np.transpose(data.reshape(points, points)), origin='bottom',
+                  extent=(x.min(), x.max(), y.min(), y.max()))
+        ax.contour(x, y, np.transpose(data_fitted.reshape(points, points)), 2, colors='k')
+        ax.set_title('(x,y)=(%.4f, %.4f)' %(popt[1], popt[2]), fontsize = 15)
+        fig.colorbar(im)
+
+
+def gaussian2D((x, y), amplitude, xo, yo, sigma_x, sigma_y, theta, offset):
+    xo = float(xo)
+    yo = float(yo)
+    a = (np.cos(theta)**2)/(2*sigma_x**2) + (np.sin(theta)**2)/(2*sigma_y**2)
+    b = -(np.sin(2*theta))/(4*sigma_x**2) + (np.sin(2*theta))/(4*sigma_y**2)
+    c = (np.sin(theta)**2)/(2*sigma_x**2) + (np.cos(theta)**2)/(2*sigma_y**2)
+    g = offset + amplitude*np.exp( - (a*((x-xo)**2) + 2*b*(x-xo)*(y-yo) + c*((y-yo)**2)))
+    return g.ravel()
