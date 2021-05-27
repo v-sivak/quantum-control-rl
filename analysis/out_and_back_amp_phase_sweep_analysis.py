bde723a87d95acbe0fdaff8782f5e1c23f3118c5
Alec Eickbusch <vladimir.sivak@yale.edu>
2021-05-26 21:17:56 -0400

    Automated out_and_back sweep from outside gui

--- analysis/out_and_back_amp_phase_sweep_analysis.py
+++ analysis/out_and_back_amp_phase_sweep_analysis.py
@@ -4,7 +4,6 @@ Created on Wed Dec 16 14:07:06 2020
 
 @author: qulab
 """
-
 import os
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -13,51 +12,56 @@ from scipy.optimize import curve_fit
 from fpga_lib import *
 from fpga_lib.dsl.result import Results
 
-
-## load results of old experiment
-#exp_dir = r'D:\DATA\exp\gkp_exp.CD_gate.out_and_back_amp_phase_sweep\archive'
-#fname = '20210101.h5'
-#grp_name = '13'
-#
-#file_name = os.path.join(exp_dir, fname)
-#res = Results.create_from_file(file_name, grp_name)
-#
-#time = 160 * 1e-9 
-## 160 ns is actual delay, 2*12 is due to padding of the fast displacements 
-## to match minimum pulse length of 24 ns. there is half padding on forward and
-## backward displacements.
-#freq_e = res['sz_e_postselected_e_fit_gaussian_results'] / 360.0 / time # Hz
-#freq_g = res['sz_g_postselected_g_fit_gaussian_results'] / 360.0 / time # Hz
-#
-#fig, ax = plt.subplots(1,1)
-#ax.set_xlabel('nbar')
-#ax.set_ylabel('Frequency (kHz)')
-#plt.plot(freq_e.ax_data[0], freq_e.data*1e-3, marker='.')
-#plt.plot(freq_g.ax_data[0], freq_g.data*1e-3, marker='.')
-
-
-
-exp_dir = r'D:\DATA\exp\gkp_exp.CD_gate.out_and_back_amp_phase_sweep\archive'
-fname = '20210423.h5'
-file_name = os.path.join(exp_dir, fname)
-
-
-qubit_states = ['g', 'e']
-groups = [1, 3, 5, 8, 7, 6, 4, 2, 0]
-time = np.array([48, 72, 100, 120, 144, 168, 192, 216, 240]) # [ns]
-nbar = np.linspace(4, 400, 45)
-
-time_points = len(time)
-nbar_points = len(nbar)
-
-phase = {s:[] for s in qubit_states}
-for i in range(time_points):
-    grp_name = str(groups[i])
-    res = Results.create_from_file(file_name, grp_name)
-    phase['e'].append(res['sz_e_postselected_e_fit_gaussian_results'].data)
-    phase['g'].append(res['sz_g_postselected_g_fit_gaussian_results'].data)
+# Run a series of experiments for different values of the delay time 'tau'.
+# In each experiment, cavity rotation angle accumulated during this time
+# will be extracted as a function of nbar. 
+if 1:
+    from fpga_lib.scripting import wait_complete, get_last_results, connect_to_gui, get_gui, get_experiment
+    connect_to_gui()
+    get_gui()
+    qubit_states = ['g', 'e']
+    time = np.array([48, 72, 96, 120, 144, 168, 192, 216, 240])
+#    time = np.array([48, 144, 240])
+    nbar_range = (4, 400, 45)
+    nbar_points = nbar_range[-1]
+    nbar = np.linspace(*nbar_range)
+    phase = {s:[] for s in qubit_states}
     
-phase = {s:np.array(phase[s]) for s in qubit_states}
+    for tau in time:       
+        exp = get_experiment('gkp_exp.CD_gate.out_and_back_amp_phase_sweep', from_gui=True)
+        exp.set_params({'static_tau' : tau,
+                        'nbar_range' : nbar_range})
+        exp.run()
+        wait_complete(exp)
+        res = get_last_results(exp)
+        phase['e'].append(res['sz_e_postselected_e_fit_gaussian_results'].data)
+        phase['g'].append(res['sz_g_postselected_g_fit_gaussian_results'].data)
+    
+    phase = {s:np.array(phase[s]) for s in qubit_states}
+    
+
+# If the experiments were done manually from GUI, this will load them from archive
+if 0:
+    exp_dir = r'D:\DATA\exp\gkp_exp.CD_gate.out_and_back_amp_phase_sweep\archive'
+    fname = '20210423.h5'
+    file_name = os.path.join(exp_dir, fname)
+    
+    qubit_states = ['g', 'e']
+    groups = [1, 3, 5, 8, 7, 6, 4, 2, 0]
+    time = np.array([48, 72, 100, 120, 144, 168, 192, 216, 240]) # [ns]
+    nbar = np.linspace(4, 400, 45)
+    
+    time_points = len(time)
+    nbar_points = len(nbar)
+    
+    phase = {s:[] for s in qubit_states}
+    for i in range(time_points):
+        grp_name = str(groups[i])
+        res = Results.create_from_file(file_name, grp_name)
+        phase['e'].append(res['sz_e_postselected_e_fit_gaussian_results'].data)
+        phase['g'].append(res['sz_g_postselected_g_fit_gaussian_results'].data)
+        
+    phase = {s:np.array(phase[s]) for s in qubit_states}
 
 
 # fit rotation frequency
@@ -71,6 +75,10 @@ for s in qubit_states:
         popt, pcov = curve_fit(linear, time, phase[s][:,i])
         freq_fits[s].append(popt[0])
         offset_fits[s].append(popt[1])
+
+np.save(r'Y:\tmp\for Vlad\from_vlad\nbar.npy', nbar)
+np.save(r'Y:\tmp\for Vlad\from_vlad\freq_e.npy', freq_fits['e'])
+np.save(r'Y:\tmp\for Vlad\from_vlad\freq_g.npy', freq_fits['g'])
 
 # Plot phase vs time for each alpha
 fig, ax = plt.subplots(1,1, figsize=(9,5))
@@ -88,20 +96,11 @@ for s in qubit_states:
                 linestyle='-', marker=None, color=color)
 
 
-# Plot rotation frequency vs nbar
-fig, ax = plt.subplots(1,1)
-ax.set_xlabel('nbar')
-ax.set_ylabel('Rotation frequency (kHz)')
-freq_fits = {s:np.array(freq_fits[s]) for s in qubit_states}
-for s in qubit_states:
-    ax.plot(nbar, freq_fits[s]*1e-3, marker='o')
-
-
 # Plot linear fit for all nbars in separate panels
 fig, axes = plt.subplots(7,7, sharex=True, sharey=True, figsize=(25,14))
 axes = axes.ravel()
 for j in range(45):
-    for s in ['g']:
+    for s in ['e']:
         for i in [j]:
             mean_phase = phase[s][:,i]
             axes[j].plot(time, mean_phase, color='black', linestyle='none', marker='o')
@@ -109,6 +108,71 @@ for j in range(45):
                     linestyle='-', marker=None, color='red')
 plt.tight_layout()
 
-np.save(r'Y:\tmp\for Vlad\from_vlad\nbar.npy', nbar)
-np.save(r'Y:\tmp\for Vlad\from_vlad\freq_e.npy', freq_fits['e'])
-np.save(r'Y:\tmp\for Vlad\from_vlad\freq_g.npy', freq_fits['g'])
+
+
+
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+
+freq_g, freq_e = np.array(freq_fits['g']), np.array(freq_fits['e'])
+# Plot cavity rotation frequency as a function of nbar
+fig, ax = plt.subplots(1,1,figsize=(3.375,2), dpi=200)
+ax.set_xlabel('nbar')
+ax.set_ylabel('Rotation freq (kHz)')
+ax.plot(nbar, freq_g*1e-3, marker='.', linestyle='none', label='g')
+ax.plot(nbar, freq_e*1e-3, marker='.', linestyle='none', label='e')
+ax.legend(loc='lower right')
+plt.tight_layout()
+
+
+# Plot average rotation frequency and fit Kerr and detuning
+avg_freq = (freq_g + freq_e)/2.0
+def avg_freq_fit_func(n, Kerr, Delta):
+    return  Delta + Kerr * n
+fit_pts = 45
+popt, pcov = curve_fit(avg_freq_fit_func, nbar[:fit_pts], avg_freq[:fit_pts])
+Kerr, Delta = popt
+
+fig, ax = plt.subplots(1,1,figsize=(3.375,2.2), dpi=200)
+ax.set_xlabel('nbar')
+ax.set_ylabel('Avg rotation freq (kHz)')
+ax.plot(nbar, avg_freq*1e-3, marker='.',linestyle='none')
+ax.plot(nbar[:fit_pts], avg_freq_fit_func(nbar[:fit_pts], *popt)*1e-3,
+        label=r'K=%.0f Hz, $\Delta$= %.1f kHz' %(Kerr,Delta*1e-3))
+ax.legend()
+plt.tight_layout()
+
+
+# Plot difference rotation frequency and fit chi and chi_prime
+diff_freq = freq_g - freq_e
+def diff_freq_fit_func(n, chi, chi_prime):
+    return  chi + chi_prime * n
+popt, pcov = curve_fit(diff_freq_fit_func, nbar[:fit_pts], diff_freq[:fit_pts])
+chi, chi_prime = popt
+
+fig, ax = plt.subplots(1,1,figsize=(3.375,2.2), dpi=200)
+ax.set_xlabel('nbar')
+ax.set_ylabel('Diff rotation freq (kHz)')
+ax.plot(nbar, diff_freq*1e-3, marker='.',linestyle='none')
+ax.plot(nbar[:fit_pts], diff_freq_fit_func(nbar[:fit_pts], *popt)*1e-3,
+        label='chi=%.0f kHz, chi_prime= %.0f Hz' %(chi*1e-3,chi_prime))
+ax.legend()
+plt.tight_layout()
+
+
+# Plot quartic fits to 'e' and 'g' rotation frequency
+def quartic_fit(n, c0, c1, c2, c3, c4):
+    return c0 + c1*n + c2*n**2 + c3*n**3 + c4*n**4
+popt_g, _ = curve_fit(quartic_fit, nbar, freq_g)
+popt_e, _ = curve_fit(quartic_fit, nbar, freq_e)
+
+fig, ax = plt.subplots(1,1,figsize=(3.375,2), dpi=200)
+ax.set_xlabel('nbar')
+ax.set_ylabel('Rotation freq (kHz)')
+ax.plot(nbar, freq_g*1e-3, marker='.', linestyle='none', label='g')
+ax.plot(nbar, freq_e*1e-3, marker='.', linestyle='none', label='e')
+ax.plot(nbar, quartic_fit(nbar, *popt_g)*1e-3)
+ax.plot(nbar, quartic_fit(nbar, *popt_e)*1e-3)
+ax.legend(loc='lower right')
+plt.tight_layout()
