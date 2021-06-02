0a5d6f6033912087ee6706c29292ea3bb0fea1a6
Vladimir Sivak <sivak.volodymyr@gmail.com>
2021-06-02 14:35:59 -0400

    Update qubit demo training/plotting

--- qubit_flip_demo_plotting.py
+++ qubit_flip_demo_plotting.py
@@ -61,6 +61,42 @@ anim.save(savename, writer='imagemagick', dpi=600)
 
 # ---------------------------------------------------------------------------
 # ---------------------------------------------------------------------------
+# Create animation with gaussian policies changing as epochs go on, also 
+# showing which policies were sampled from these gaussians.
+fig, ax = plt.subplots(1, 1, figsize=(3.375,2), dpi=200)
+savename = os.path.join(folder_name, r'gaussians.pdf')
+plt.tight_layout()
+ax.set_ylim(0, 14)
+ax.set_xlim(-1, 1)
+ax.set_xlabel(r'Action, $a$')
+ax.set_ylabel('Probability density')
+ax.set_xticks([-1, -0.5, 0, 0.5, 1])
+ax.plot(np.ones(2)*0.5, [0,14], linestyle='--', color='k')
+ax.plot(-np.ones(2)*0.5, [0,14], linestyle='--', color='k')
+
+epochs = len(log['mean'])
+batch = len(log['train_actions'][0])
+
+def gaussian(x, _mean, _sigma):
+    return 1/np.sqrt(2*np.pi*_sigma**2) * np.exp(-(x-_mean)**2/2/_sigma**2)
+
+palette = plt.get_cmap('tab10')
+
+for i, e in enumerate([0, 20, 30, 40, 50]):
+    xs = np.linspace(-1, 1, 301)
+    ys = gaussian(xs, log['mean'][e], log['sigma'][e])
+    ax.plot(xs, ys, color=palette(i))
+    e_points = e if e<50 else 49
+    points = log['train_actions'][e_points]
+    vals = gaussian(points, log['mean'][e], log['sigma'][e])
+    ax.plot(points, vals, linestyle='none', marker='.', color=palette(i),
+            label=e)
+ax.legend()
+fig.savefig(savename, dpi=600)
+
+
+# ---------------------------------------------------------------------------
+# ---------------------------------------------------------------------------
 # Create animaiton with measurement outcomes (0 or 1) appearing in each epoch
 fig, ax = plt.subplots(1,1, figsize=(10,5))
 savename = os.path.join(folder_name, r'bits.gif')
@@ -103,38 +139,68 @@ fig.savefig(savename, bbox_inches='tight')
 
 # ---------------------------------------------------------------------------
 # ---------------------------------------------------------------------------
-# Cretae animation showing how Gradient Ascent would work on this problem
+# Create a static figure with measurement outcomes as colored squares
+fig, ax = plt.subplots(1,1, figsize=(3.375,2), dpi=200)
+savename = os.path.join(folder_name, r'bit_map.pdf')
+# plt.axis('off')
+plt.tight_layout()
+vals = np.where(log['train_rewards']==1, 0.6, -0.9)
+
+ax.pcolormesh(np.transpose(vals), cmap='RdYlGn', vmin=-1, vmax=1)
+
+ax.set_xticks(np.arange(0, vals.shape[0], 1), minor=True)
+ax.set_yticks(np.arange(0, vals.shape[1], 1), minor=True)
+ax.grid(which='both', color='w', linestyle='-')
+ax.set_aspect('equal')
+
+ax.set_ylabel('Batch')
+ax.set_xlabel('Epoch')
+fig.savefig(savename, dpi=600)
+
+# ---------------------------------------------------------------------------
+# ---------------------------------------------------------------------------
+# Create animation showing how Gradient Ascent would work on this problem
 fig, ax = plt.subplots(1,1, figsize=(3.375,2))
-ax.set_xlabel('Iteration')
+ax.set_xlabel('Iteration, k')
 ax.set_ylabel('Fidelity')
 ax.set_xlim(0,20)
 ax.set_ylim(0,1)
+# ax.set_ylim(1e-5,1)
+# ax.set_yscale('log')
 ax.set_xticks([0,5,10,15,20])
 savename = os.path.join(folder_name, r'GD.gif')
 plt.tight_layout()
 
 a = [0.02]
-lr = 1e-2
-steps = 20
+lr = 0.9e-2
+steps = 22
 
 for i in range(steps):
     a_new = a[-1] + lr * 2*np.pi*np.sin(2*np.pi*a[-1])
     a.append(a_new)
+a = np.array(a)
 
-fidelity = np.sin(np.pi*np.array(a))**2
+fidelity = np.sin(np.pi*a)**2
+fidelity_true = np.sin(np.pi*a**1.2)**2
 
-line = ax.plot(np.zeros(2), np.zeros(2))[0]
-text1 = ax.text(12, 0.5, 'a=%.3f'%a[0])
+# line2 = ax.plot(np.zeros(2), np.zeros(2), color='red')[0]
+line1 = ax.plot(np.zeros(2), np.zeros(2), color='green')[0]
+text1 = ax.text(12, 0.5, '$a$=%.3f'%a[0])
 text2 = ax.text(12, 0.35, 'F=%.3f'%fidelity[0])
+# text3 = ax.text(12, 0.2, 'F=%.3f'%fidelity[0], color='red')
 
 def update(i):
-    line.set_xdata(np.arange(i))
-    line.set_ydata(fidelity[:i])
+    i = min(i, steps-1)
+    line1.set_xdata(np.arange(i))
+    line1.set_ydata(fidelity[:i])
+    # line2.set_xdata(np.arange(i))
+    # line2.set_ydata(fidelity_true[:i])
     text1.set_text('a=%.3f'%a[i])
     text2.set_text('F=%.5f'%fidelity[i])
-    return line, text1, text2
+    # text3.set_text('F=%.5f'%fidelity_true[i])
+    return line1, text1, text2
 
-anim = FuncAnimation(fig, update, frames=np.arange(0, steps), interval=200)
+anim = FuncAnimation(fig, update, frames=np.arange(0, steps+10), interval=300)
 anim.save(savename, writer='imagemagick', dpi=600)
 
 
@@ -145,7 +211,7 @@ anim.save(savename, writer='imagemagick', dpi=600)
 # estimation on a static protocol, and sample complexity of RL training 
 # required to achieve the same fidelity. It demonstrates that the agent can
 # reach close to the shot-noise limit.
-folder_name = r'E:\data\gkp_sims\PPO\simple_demo\sweep'
+folder_name = r'E:\data\gkp_sims\PPO\simple_demo\sweep11'
 all_trajectories = []
 N_trajectories = len(os.listdir(folder_name))-1
 for i in range(N_trajectories):
@@ -159,6 +225,7 @@ batch = len(log['train_rewards'][0])
 fig, ax = plt.subplots(1,1, figsize=(3.375,2.5), dpi=200)
 savename = os.path.join(folder_name, 'sample_complexity.png')
 ax.set_ylim(1e-5,1.2)
+# ax.set_xlim(0,60)
 ax.set_yscale('log')
 ax.set_xlabel('Epoch')
 ax.set_ylabel('Infidelity')
@@ -166,15 +233,17 @@ for i in range(N_trajectories):
     ax.plot(eval_epochs, 1-all_trajectories[i], alpha=0.3, linestyle='--')
 
 median = np.median(all_trajectories, axis=0)
+logmean = 1-np.exp(np.mean(np.log(1-np.array(all_trajectories)),axis=0))
 
 ax.plot(eval_epochs, 1/(1+eval_epochs*batch), color='k', label='shot noise limit')
-ax.plot(eval_epochs, 1-median, color='k', linestyle='--', label='median')
+ax.plot(eval_epochs, 1-median, color='r', linestyle='--', label='median')
+ax.plot(eval_epochs, 1-logmean, color='k', linestyle='--', label='logmean')
 ax.legend(loc='lower left')
 
 msmt = lambda x: x*batch
 ax2 = ax.secondary_xaxis('top', functions=(msmt, msmt))
 ax2.set_xlabel('# of measurements, M')
-ax2.set_xticks([0,300,600,900,1200,1500])
+# ax2.set_xticks([0,300,600,900,1200,1500])
 
 plt.tight_layout()
 
--- qubit_flip_demo_training.py
+++ qubit_flip_demo_training.py
@@ -13,149 +13,152 @@ import tensorflow_probability as tfp
 import numpy as np
 import matplotlib.pyplot as plt
 
+rootdir = r'E:\data\gkp_sims\PPO\simple_demo\sweep11'
 
+for SEED in range(100):
 
-# trainable variables
-actions = ['theta']
-mean = {s : tf.Variable(tf.random.normal([], stddev=0.05), name='mean_'+s) for s in actions}
-sigma = {s : tf.Variable(0.5, name='sigma_'+s) for s in actions}
-baseline = tf.Variable(0.0, name='baseline')
-
-
-algo = 'PPO'
-B = 30 # batch_size
-EPOCHS = 50
-eval_interval = 1
-lr = 5e-3 # 1e-2 for SGD
-policy_steps = 20
-
-log_prob_clipping = 10.
-gradient_clipping = 1.
-importance_ratio_eps = 0.2
-
-# optimizer = tf.optimizers.Adam(learning_rate=lr)
-optimizer = tf.optimizers.SGD(learning_rate=lr)
-
-def reward_sampler(a, epoch, _type):
-    """
-    This implements a unitary U(a) = exp(-i*pi*a*sigma_x) on the state |0>,
-    and then simply samples sigma_z measurement outcomes with the resulting
-    state, and returns -sigma_z as a reward. THis will encaurage the agent
-    to prepare |1> state. The ideal action would correspond to a=0.5
-    """
-    a_np = {s : np.array(a[s] if _type=='collection' else [a[s]]) for s in actions}
+    # trainable variables
+    actions = ['theta']
+    mean = {s : tf.Variable(tf.random.normal([], stddev=0.05), name='mean_'+s) for s in actions}
+    sigma = {s : tf.Variable(0.5, name='sigma_'+s) for s in actions}
+    baseline = tf.Variable(0.0, name='baseline')
     
-    scale = 2*np.pi
-    theta = a_np['theta'] * scale
-    probs = np.sin(theta/2)**2
-    if _type=='collection':
-        P = tfp.distributions.Bernoulli(probs=probs)
-        sigma_z = 1 - 2 * P.sample()
-        rewards = -sigma_z
-    elif _type=='evaluation':
-        sigma_z = 1 - 2 * probs
-        print('EVAL NOW %.3f' %float(probs))
-        print('EVAL NOW %.3f' %float(sigma_z))
-        rewards = -sigma_z
-    return tf.cast(rewards, tf.float32)
-
-
-def evaluation(epoch, log=None):
-    value = lambda a: a.value()
-    eval_action = tf.nest.map_structure(value, mean)
-    R_eval = reward_sampler(eval_action, epoch, 'evaluation')
-    print('Deterministic policy: %.3f' %float(tf.reduce_mean(R_eval)))
-    for s in actions:
-        train_vars_tuple = (float(mean[s].numpy()), float(sigma[s].numpy()))
-        print(s+': %.5f +- %.5f' %train_vars_tuple)
-    if log is not None:
-        log['eval_rewards'].append(np.array(R_eval))
-        log['eval_epochs'].append(epoch)
-        log['mean'].append(mean['theta'].numpy())
-        log['sigma'].append(sigma['theta'].numpy())
-
-def compute_log_prob(action, mean, sigma):
-    sigma_eps = 1e-5 # for mumerical stability
-    log_prob = 0.
-    for s in action.keys():
-        log_prob += - tf.math.log(tf.math.abs(sigma[s]) + sigma_eps) \
-            - 0.5 * (action[s] - mean[s])**2 / (sigma[s]**2 + sigma_eps)
-    return log_prob
-
-log = dict(train_rewards=[], train_epochs=[], eval_rewards=[], eval_epochs=[],
-           train_actions=[], mean=[], sigma=[])
-
-evaluation(0, log) # evaluate policy once before training
-
-for epoch in range(1,EPOCHS+1):
-    # sample a batch of actions from Gaussian policy
-    N = {s : tfp.distributions.Normal(loc=mean[s], scale=sigma[s]) for s in actions}
-    a = {s : N[s].sample(B) for s in actions}
-
-    # collect those rewards
-    R =  reward_sampler(a, epoch, 'collection')
     
-    # log prob according to old policy (required for importance ratio)
-    if epoch == 1: mean_old, sigma_old = mean, sigma 
-    log_prob_old = compute_log_prob(a, mean_old, sigma_old)
-    log_prob_old = tf.clip_by_value(log_prob_old, -log_prob_clipping, log_prob_clipping)
-    mean_old = tf.nest.map_structure(tf.identity, mean)
-    sigma_old = tf.nest.map_structure(tf.identity, sigma)
+    algo = 'PPO'
+    B = 30 # batch_size
+    EPOCHS = 100
+    eval_interval = 1
+    lr = 5e-3 # 1e-2 for SGD
+    policy_steps = 20
     
-    # calculate policy loss and do several gradient updates
-    for i in range(policy_steps):
-        with tf.GradientTape() as tape:
-            # log prob according to the current policy
-            log_prob = compute_log_prob(a, mean, sigma)
-            log_prob = tf.clip_by_value(log_prob, -log_prob_clipping, log_prob_clipping)
-            
-            A = R - baseline # action advantages       
+    log_prob_clipping = 10.
+    gradient_clipping = 1.
+    importance_ratio_eps = 0.20
+    value_loss_coeff = 0.5
     
-            if algo == 'REINFORCE':
-                policy_loss_batch = - A * log_prob
+    optimizer = tf.optimizers.Adam(learning_rate=lr)
+    # optimizer = tf.optimizers.SGD(learning_rate=lr)
+    
+    def reward_sampler(a, epoch, _type):
+        """
+        This implements a unitary U(a) = exp(-i*pi*a*sigma_x) on the state |0>,
+        and then simply samples sigma_z measurement outcomes with the resulting
+        state, and returns -sigma_z as a reward. THis will encaurage the agent
+        to prepare |1> state. The ideal action would correspond to a=0.5
+        """
+        a_np = {s : np.array(a[s] if _type=='collection' else [a[s]]) for s in actions}
+        
+        scale = 2*np.pi
+        theta = a_np['theta'] * scale
+        probs = np.sin(theta/2)**2
+        if _type=='collection':
+            P = tfp.distributions.Bernoulli(probs=probs)
+            sigma_z = 1 - 2 * P.sample()
+            rewards = -sigma_z
+        elif _type=='evaluation':
+            sigma_z = 1 - 2 * probs
+            print('EVAL NOW %.3f' %float(probs))
+            print('EVAL NOW %.3f' %float(sigma_z))
+            rewards = -sigma_z
+        return tf.cast(rewards, tf.float32)
+    
+    
+    def evaluation(epoch, log=None):
+        value = lambda a: a.value()
+        eval_action = tf.nest.map_structure(value, mean)
+        R_eval = reward_sampler(eval_action, epoch, 'evaluation')
+        print('Deterministic policy: %.3f' %float(tf.reduce_mean(R_eval)))
+        for s in actions:
+            train_vars_tuple = (float(mean[s].numpy()), float(sigma[s].numpy()))
+            print(s+': %.5f +- %.5f' %train_vars_tuple)
+        if log is not None:
+            log['eval_rewards'].append(np.array(R_eval))
+            log['eval_epochs'].append(epoch)
+            log['mean'].append(mean['theta'].numpy())
+            log['sigma'].append(sigma['theta'].numpy())
+    
+    def compute_log_prob(action, mean, sigma):
+        sigma_eps = 1e-5 # for mumerical stability
+        log_prob = 0.
+        for s in action.keys():
+            log_prob += - tf.math.log(tf.math.abs(sigma[s]) + sigma_eps) \
+                - 0.5 * (action[s] - mean[s])**2 / (sigma[s]**2 + sigma_eps)
+        return log_prob
+    
+    log = dict(train_rewards=[], train_epochs=[], eval_rewards=[], eval_epochs=[],
+               train_actions=[], mean=[], sigma=[])
+    
+    evaluation(0, log) # evaluate policy once before training
+    
+    for epoch in range(1,EPOCHS+1):
+        # sample a batch of actions from Gaussian policy
+        N = {s : tfp.distributions.Normal(loc=mean[s], scale=sigma[s]) for s in actions}
+        a = {s : N[s].sample(B) for s in actions}
+    
+        # collect those rewards
+        R =  reward_sampler(a, epoch, 'collection')
+        
+        # log prob according to old policy (required for importance ratio)
+        if epoch == 1: mean_old, sigma_old = mean, sigma 
+        log_prob_old = compute_log_prob(a, mean_old, sigma_old)
+        log_prob_old = tf.clip_by_value(log_prob_old, -log_prob_clipping, log_prob_clipping)
+        mean_old = tf.nest.map_structure(tf.identity, mean)
+        sigma_old = tf.nest.map_structure(tf.identity, sigma)
+        
+        # calculate policy loss and do several gradient updates
+        for i in range(policy_steps):
+            with tf.GradientTape() as tape:
+                # log prob according to the current policy
+                log_prob = compute_log_prob(a, mean, sigma)
+                log_prob = tf.clip_by_value(log_prob, -log_prob_clipping, log_prob_clipping)
                 
-            if algo == 'PPO':
-                importance_ratio = tf.math.exp(log_prob - log_prob_old)
-                importance_ratio_clip = tf.clip_by_value(importance_ratio, 
-                            1-importance_ratio_eps, 1+importance_ratio_eps)
-                policy_loss_batch = -tf.minimum(importance_ratio*A, importance_ratio_clip*A)
-            
-            policy_loss = tf.reduce_mean(policy_loss_batch) # reduce over batch
-            value_loss = tf.reduce_mean(A**2)
-            loss = policy_loss + value_loss
-            
-            grads = tape.gradient(loss, tape.watched_variables())
-            grads = tf.clip_by_value(grads, -gradient_clipping, gradient_clipping)
-            optimizer.apply_gradients(zip(grads, tape.watched_variables()))
-    print('Epoch %d: %.3f' %(epoch, float(tf.reduce_mean(R))))
-    log['train_rewards'].append(np.array(R))
-    log['train_actions'].append(np.array(a['theta']))
-    log['train_epochs'].append(epoch)
-    if epoch % eval_interval == 0: evaluation(epoch, log)
-
-
-# Plot training progress
-from plotting import plot_config
-fix, ax = plt.subplots(1,1, figsize=(3.375,2), dpi=200)
-plt.grid()
-ax.set_xlabel('Epoch')
-ax.set_ylabel('Reward')
-ax.set_ylim(-1,1)
-R_mean = np.mean(log['train_rewards'], axis=1)
-ax.plot(log['train_epochs'], R_mean, label='Stochastic policy')
-ax.plot(log['eval_epochs'], log['eval_rewards'], label='Deterministic policy')
-ax.legend(loc='best')
-plt.tight_layout()
-
-
-# Plot gaussian policies
-def gaussian(x, _mean, _sigma):
-    return 1/np.sqrt(2*np.pi*_sigma**2) * np.exp(-(x-_mean)**2/2/_sigma**2)
-
-fig, ax = plt.subplots(1, 1, figsize=(3.375,2), dpi=200)
-xs = np.linspace(-1, 1, 201)
-for i in range(len(log['mean'])):
-    ax.plot(xs, gaussian(xs, log['mean'][i], log['sigma'][i]))
-
-
-np.savez(r'E:\data\gkp_sims\PPO\simple_demo\3\data.npz', **log)
+                A = R - baseline # action advantages       
+        
+                if algo == 'REINFORCE':
+                    policy_loss_batch = - A * log_prob
+                    
+                if algo == 'PPO':
+                    importance_ratio = tf.math.exp(log_prob - log_prob_old)
+                    importance_ratio_clip = tf.clip_by_value(importance_ratio, 
+                                1-importance_ratio_eps, 1+importance_ratio_eps)
+                    policy_loss_batch = -tf.minimum(importance_ratio*A, importance_ratio_clip*A)
+                
+                policy_loss = tf.reduce_mean(policy_loss_batch) # reduce over batch
+                value_loss = tf.reduce_mean(A**2)
+                loss = policy_loss + value_loss_coeff * value_loss
+                
+                grads = tape.gradient(loss, tape.watched_variables())
+                grads = tf.clip_by_value(grads, -gradient_clipping, gradient_clipping)
+                optimizer.apply_gradients(zip(grads, tape.watched_variables()))
+        print('Epoch %d: %.3f' %(epoch, float(tf.reduce_mean(R))))
+        log['train_rewards'].append(np.array(R))
+        log['train_actions'].append(np.array(a['theta']))
+        log['train_epochs'].append(epoch)
+        if epoch % eval_interval == 0: evaluation(epoch, log)
+    
+    
+    # # Plot training progress
+    # from plotting import plot_config
+    # fix, ax = plt.subplots(1,1, figsize=(3.375,2), dpi=200)
+    # plt.grid()
+    # ax.set_xlabel('Epoch')
+    # ax.set_ylabel('Reward')
+    # ax.set_ylim(-1,1)
+    # R_mean = np.mean(log['train_rewards'], axis=1)
+    # ax.plot(log['train_epochs'], R_mean, label='Stochastic policy')
+    # ax.plot(log['eval_epochs'], log['eval_rewards'], label='Deterministic policy')
+    # ax.legend(loc='best')
+    # plt.tight_layout()
+    
+    
+    # # Plot gaussian policies
+    # def gaussian(x, _mean, _sigma):
+    #     return 1/np.sqrt(2*np.pi*_sigma**2) * np.exp(-(x-_mean)**2/2/_sigma**2)
+    
+    # fig, ax = plt.subplots(1, 1, figsize=(3.375,2), dpi=200)
+    # xs = np.linspace(-1, 1, 201)
+    # for i in range(len(log['mean'])):
+    #     ax.plot(xs, gaussian(xs, log['mean'][i], log['sigma'][i]))
+    
+    filename = os.path.join(rootdir, 'data'+str(SEED)+'.npz')
+    np.savez(filename, **log)
