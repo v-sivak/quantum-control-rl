716cb4bf525567e90f82b9d1d727780e802fe53e
Alec Eickbusch <vladimir.sivak@yale.edu>
2021-07-07 08:58:23 -0400

    Add Kerr-cancelling drive experiment

---
+++ analysis/out_and_back_Kerr_cancel.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 22 15:47:06 2021
+
+@author: qulab
+"""
+import os
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import curve_fit
+from fpga_lib import *
+from fpga_lib.dsl.result import Results
+import plot_config
+
+
+from fpga_lib.scripting import wait_complete, get_last_results, connect_to_gui, get_gui, get_experiment
+connect_to_gui()
+get_gui()
+#time = np.array([48, 72, 96, 120, 144, 168, 192, 216, 240])
+time = np.array([100,200,300,400,500,600])
+nbar_range = (4, 80, 11)
+nbar_points = nbar_range[-1]
+nbar = np.linspace(*nbar_range)
+phase = []
+
+for tau in time:       
+    exp = get_experiment('gkp_exp.fpga_exp.out_and_back_Kerr_cancel', from_gui=True)
+    exp.set_params({'Kerr_pump_time_ns' : tau,
+                    'nbar_range' : nbar_range,
+                    'do_e' : False})
+    exp.run()
+    wait_complete(exp)
+    res = get_last_results(exp)
+    phase.append(res['sz_g_postselected_g_fit_gaussian_results'].data)
+
+phase = np.array(phase)
+
+
+
+# fit rotation frequency
+def linear(t, freq, offset):
+    return 360*freq*t*1e-9 + offset
+
+freq_fits = []
+offset_fits = []
+for i in range(nbar_points):
+    popt, pcov = curve_fit(linear, time, phase[:,i])
+    freq_fits.append(popt[0])
+    offset_fits.append(popt[1])
+
+freq_fits = np.array(freq_fits)
+
+#np.save(r'Y:\tmp\for Vlad\from_vlad\nbar.npy', nbar)
+#np.save(r'Y:\tmp\for Vlad\from_vlad\freq_g.npy', freq_fits)
+
+
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+
+
+
+# Plot "Phase vs nbar" for all times in one panel.
+fig, ax = plt.subplots(1,1, figsize=(3.375,2), dpi=200)
+ax.set_xlabel('nbar')
+ax.set_ylabel('Return phase (deg)')
+cmap = plt.get_cmap('Spectral_r')
+norm = mpl.colors.Normalize(vmin = min(time), vmax = max(time))
+
+for i in range(len(time)):
+    mean_phase = phase[i,:]
+    color = cmap(norm(time[i]))
+    ax.plot(nbar, mean_phase, color=color, linestyle='none', marker='.',
+            label=str(time[i]))
+ax.legend()
+plt.grid()
+plt.tight_layout()
+
+
+# Plot "rotation frequency vs nbar" and linear fit with extracted Kerr
+def linear_fit(n, c0, c1):
+    return c0 + c1*n
+popt_g, _ = curve_fit(linear_fit, nbar, freq_fits)
+
+fig, ax = plt.subplots(1,1,figsize=(3.375,2), dpi=200)
+ax.set_xlabel('nbar')
+ax.set_ylabel('Rotation freq (kHz)')
+ax.plot(nbar, freq_fits*1e-3, marker='.', linestyle='none')
+ax.plot(nbar, linear_fit(nbar, *popt_g)*1e-3, label=r'$K_g$=%.0f Hz' %popt_g[1])
+ax.legend(loc='lower right')
+plt.tight_layout()
+
+
+
+# Plot "phase vs time" for all nbars in one panel. This shows that rotation
+# is indeed linear in time, so it can be characterized by a angular frequency
+fig, ax = plt.subplots(1,1, figsize=(3.375,2), dpi=200)
+ax.set_xlabel('Time (ns)')
+ax.set_ylabel('Return phase (deg)')
+cmap = plt.get_cmap('Spectral_r')
+norm = mpl.colors.Normalize(vmin = min(nbar), vmax = max(nbar))
+
+for i in range(nbar_points):
+    mean_phase = phase[:,i]
+    color = cmap(norm(nbar[i]))
+    ax.plot(time, mean_phase, color=color, linestyle='none', marker='.',
+            markersize=10)
+    ax.plot(time, linear(time, freq_fits[i], offset_fits[i]), 
+            linestyle='-', marker=None, color=color)
+
+
+# Plot "phase vs time" for all nbars in separate panels
+fig, axes = plt.subplots(3,4, sharex=True, sharey=True, figsize=(25,14))
+axes = axes.ravel()
+for j in range(11):
+    for s in ['g']:
+        for i in [j]:
+            mean_phase = phase[:,i]
+            axes[j].plot(time, mean_phase, color='black', linestyle='none', marker='o')
+            axes[j].plot(time, linear(time, freq_fits[i], offset_fits[i]), 
+                    linestyle='-', marker=None, color='red')
+plt.tight_layout()
---
+++ fpga_exp/out_and_back_Kerr_cancel.py
@@ -0,0 +1,206 @@
+from init_script import *
+from fpga_lib.analysis import fit
+from scipy.optimize import curve_fit
+
+class out_and_back_Kerr_cancel(FPGAExperiment):
+#    static_tau = FloatParameter(160)
+    delay_time = FloatParameter(4e6)
+    do_g = BoolParameter(True)
+    do_e = BoolParameter(True)
+    phase_range_deg = RangeParameter((-10, 20, 51))
+    nbar_range = RangeParameter((10,800,41))
+    postselect = BoolParameter(True)
+    
+    detune_Kerr_pump_MHz = FloatParameter(20)
+    Kerr_pump_time_ns = IntParameter(200)
+    Kerr_pump_amp = FloatParameter(0.05)
+    Kerr_pump_ramp_ns = IntParameter(100)
+
+#    fit_func = {'chi_kHz':'linear','detuning_kHz':'linear'}
+
+    def sequence(self):
+
+        self.cavity = cavity
+        self.cavity_1 = cavity_1
+        
+        # setup qubit mode for Kerr cancelling drive
+        self.qubit_detuned = qubit_ef
+        self.qubit_detuned.set_detune(self.detune_Kerr_pump_MHz*1e6)
+
+        #need to dynamicaly update the phase of the return pulse
+        @arbitrary_function(float, float)
+        def cos(x):
+            return np.cos(np.pi * x)
+
+        @arbitrary_function(float, float)
+        def sin(x):
+            return np.sin(np.pi * x)
+
+        # here, will assume cavity is pulse out and cavity_2 is pulse back
+        @subroutine
+        def update_amp_phase_out_and_return(amp_reg, phase_reg):
+            cx = FloatRegister()
+            sx = FloatRegister()
+            cx <<= amp_reg
+            sx <<= 0
+            sync()
+            DynamicMixer[0][0] <<= cx
+            DynamicMixer[1][1] <<= cx
+            DynamicMixer[0][1] <<= sx
+            DynamicMixer[1][0] <<= -sx
+            sync()
+            delay(2000)
+            self.cavity.load_mixer()
+            delay(2000)
+            cx <<= amp_reg*cos(phase_reg)
+            sx <<= amp_reg*sin(phase_reg)
+            sync()
+            DynamicMixer[0][0] <<= cx
+            DynamicMixer[1][1] <<= cx
+            DynamicMixer[0][1] <<= sx
+            DynamicMixer[1][0] <<= -sx
+            sync()
+            delay(2000)
+            self.cavity_1.load_mixer()
+            delay(2000)
+
+        def myexp(start='g'):
+            if start == 'e':
+                qubit.flip()
+            sync()
+            self.cavity.displace(amp='dynamic') # displace out
+            sync()
+            self.qubit_detuned.smoothed_constant_pulse(
+                    self.Kerr_pump_time_ns, amp=self.Kerr_pump_amp, 
+                    sigma_t=self.Kerr_pump_ramp_ns)
+            sync()
+            self.cavity_1.displace(amp='dynamic') # return to origin
+            sync()
+            delay(24)
+
+            sync()
+            if self.postselect:
+                readout(**{'sz_' + start + '_middle_measurement':'se'})
+                sync()
+            qubit.flip(selective=True)
+            sync()
+            readout(**{'sz_' + start:'se'})
+
+        #scan phase is in units of pi
+        phase_start = (self.phase_range_deg[0] - 180.0)/180.0
+        phase_stop = (self.phase_range_deg[1] - 180.0)/180.0
+        phase_points = self.phase_range_deg[2]
+
+        self.nbars = np.linspace(*self.nbar_range)
+        self.alphas = np.sqrt(self.nbars)
+
+        unit_amp = self.cavity.displace.unit_amp
+        dac_amps = unit_amp*self.alphas
+        f_arr = Array(dac_amps, float)
+        amp_reg = FloatRegister(0.0)
+        with scan_array(f_arr, amp_reg, axis_scale=1/unit_amp, plot_label='alpha'):
+            if self.do_g:
+                with scan_float_register(phase_start, phase_stop, phase_points,
+                        axis_scale = 180.0, plot_label='return phase, deg') as phase_reg:
+                    update_amp_phase_out_and_return(amp_reg,phase_reg)
+                    sync()
+                    myexp(start='g')
+                    sync()
+                    delay(self.delay_time)
+            if self.do_e:
+                with scan_float_register(-1*phase_start, -1*phase_stop, phase_points,
+                        axis_scale = 180.0, plot_label='return phase, deg') as phase_reg:
+                    update_amp_phase_out_and_return(amp_reg,phase_reg)
+                    sync()
+                    myexp(start='e')
+                    sync()
+                    delay(self.delay_time)
+
+
+    def fit_gaussian(self, xs, x0, sig, ofs, amp):
+        return ofs + amp * np.exp(-(xs - x0)**2 / (2 * sig**2))
+
+
+    def process_data(self):
+        ops = []
+
+        if self.do_g:
+            ops.append('sz_g')
+            if self.blocks_processed == 1:
+                self.results['sz_g'].ax_data[2] = self.results['sz_g'].ax_data[2] + 180.0
+        if self.do_e:
+            ops.append('sz_e')
+            if self.blocks_processed == 1:
+                self.results['sz_e'].ax_data[2] = self.results['sz_e'].ax_data[2] - 180.0
+
+        for op in ops:
+            if self.postselect:
+                middle_result = self.results[op + '_middle_measurement'].threshold()
+                self.results[op + '_postselected_g'] = self.results[op].postselect(middle_result,[0])[0]
+                self.results[op + '_postselected_e'] = self.results[op].postselect(middle_result,[1])[0]
+                #replace the rest of the analysis with the postselected version
+                op = op + '_postselected_g' if op == 'sz_g' else op + '_postselected_e'
+
+            gaussian_peaks = []
+            alphas = self.results[op].ax_data[1]
+            for i, alpha in enumerate(alphas):
+                phases = self.results[op].ax_data[2]
+                return_phase = self.results[op].thresh_mean().data[i]
+                if op == 'sz_g' or op == 'sz_g_postselected_g':
+                    guess = phases[np.argmax(return_phase)]
+                    p0 = (guess, 5., 0, 1.0)
+                if op == 'sz_e' or op == 'sz_e_postselected_e':
+                    guess = phases[np.argmin(return_phase)]
+                    p0 = (guess, 5., 1.0, -1.0)
+                popt, pcov = curve_fit(self.fit_gaussian, phases, return_phase, p0=p0)
+                gaussian_peaks.append(popt[0])
+
+            self.results[op + '_fit_gaussian_results'] = np.array(gaussian_peaks)
+            self.results[op + '_fit_gaussian_results'].ax_data = [self.nbars]
+
+
+#    def plot(self, fig, data):
+#        title = '%s (%s)' % (self.name, self.run_name)
+#        nbars = self.nbars
+#        ax1 = fig.add_subplot(411)
+#        sz_g_phase = self.results['sz_g_postselected_g_fit_gaussian_results'].data
+#        g_sweep_phase = self.results['sz_g_postselected_g'].ax_data[2]
+#        dx = nbars[1] - nbars[0]
+#        dy = g_sweep_phase[1] - g_sweep_phase[0]
+#        nbars_plot = np.concatenate([nbars, [nbars[-1] + dx]])
+#        g_sweep_phase= np.concatenate([g_sweep_phase, [g_sweep_phase[-1] + dy]])
+#        ax1.pcolormesh(nbars_plot - dx/2.0, g_sweep_phase - dy/2.0, self.results['sz_g_postselected_g'].thresh_mean().data.T, vmin=0, vmax=+1, cmap='seismic')
+#        ax1.plot(nbars, sz_g_phase, '.',label='data', color='lime')
+#        ax1.set_title(title)
+#
+#        ax2 = fig.add_subplot(412, sharex=ax1)
+#        sz_e_phase = self.results['sz_e_postselected_e_fit_gaussian_results'].data
+#        e_sweep_phase = self.results['sz_e_postselected_e'].ax_data[2]
+#        dy = e_sweep_phase[1] - e_sweep_phase[0]
+#        e_sweep_phase= np.concatenate([e_sweep_phase, [e_sweep_phase[-1] + dy]])
+#        ax2.pcolormesh(nbars_plot - dx/2.0, e_sweep_phase - dy/2.0, self.results['sz_e_postselected_e'].thresh_mean().data.T, vmin=0, vmax=+1, cmap='seismic')
+#        ax2.plot(nbars, sz_e_phase, '.',label='data', color='lime')
+#        ax2.set_xlabel('alpha')
+#        #ax2.grid()
+#
+#        ax3 = fig.add_subplot(413, sharex=ax2)
+#        ax3.plot(nbars, self.results['chi_kHz'].data, 'o', label='chi kHz')
+#        chi_kHz = self.fit_params['chi_kHz:b']
+#        chi_prime_kHz = self.fit_params['chi_kHz:a']
+#        predicted_chi = chi_kHz + chi_prime_kHz*nbars
+#        ax3.plot(nbars, predicted_chi, '-', label='linear fit')
+#        ax3.plot([],[], ' ', label = 'chi_kHz = %.3f ' % (chi_kHz))
+#        ax3.plot([],[], ' ', label = 'chi_prime_Hz = %.3f' % (chi_prime_kHz*1e3))
+#        ax3.grid()
+#        ax3.legend(prop={'size': 6})
+#
+#        ax4 = fig.add_subplot(414, sharex=ax3)
+#        ax4.plot(nbars, self.results['detuning_kHz'].data, 'o', label='detuning kHz')
+#        detuning_kHz = self.fit_params['detuning_kHz:b']
+#        kerr_kHz = self.fit_params['detuning_kHz:a']
+#        predicted_detuning = detuning_kHz + kerr_kHz*nbars
+#        ax4.plot(nbars, predicted_detuning, '-', label='linear fit')
+#        ax4.plot([],[], ' ', label = 'detuning_kHz = %.3f' % (detuning_kHz))
+#        ax4.plot([],[], ' ', label = 'kerr_Hz = %.3f' % (kerr_kHz*1e3))
+#        ax4.grid()
+#        ax4.legend(prop={'size': 6})
